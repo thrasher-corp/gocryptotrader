60b475f46cf58387c757162de1d979daad3333ce
GloriousCode <scott@gloriousedge.com>
2016-11-15 21:25:06 +1100

    Adds a base for the buy component

--- web/app/components/buy/buy.js
+++ web/app/components/buy/buy.js
@@ -5,7 +5,16 @@ angular.module('myApp.buy',[]).component('buy', {
    bindings: {
     message: '='
   }
-}).controller('BuyController', function ($scope, $http, Notification) {
-  alert(this.message);
+}).controller('BuyController', function ($http, Notification) {
+  //This contrioller will retrieve all enabled exchanges, 
+  //their enabled currencies and the currency's latest ask
+  //This call will be used for selling too
+  //
+  //This will allow a user to make decisions based onthe latest information to them
+  //It will auto poll every X seconds (at least until a push method is implemented)
+  //When all fields are valid, a purchase order will be sent to and handle by gocryptoServer
+
+  //Could also hard-type the exchange and currency via attributes on the component for quick use
+  //Or at lest controlled by passing data from other components/data
 });
 
