7ba0cdaf53ae9e2c9af7f5f02c4d9bd7ac30c924
Scott <scott.grant@live.com.au>
2017-01-29 21:15:07 +1100

    Converts enabled-exchanges to a component

--- web/app/views/home/home.js
+++ web/app/views/home/home.js
@@ -3,36 +3,13 @@
 angular.module('myApp.home', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: '/views/home/home.html',
-    controller: 'HomeController'
-  });
+    $routeProvider.when('/', {
+        templateUrl: '/views/home/home.html',
+        controller: 'HomeController'
+    });
 }])
 
-  .controller('HomeController', function ($scope, $http, Notification, $rootScope) {
-    $scope.selected = {};
-  $scope.getDashboardData = function() {
-    $http({
-      method: 'GET',
-      url: '/data/all-enabled-currencies'
-    }).
-    success(function (data, status, headers, config) {
-      $scope.exchanges = data.data;
-      $scope.reloadDashboardWithExchangeCurrency($scope.exchanges[0], $scope.exchanges[0].exchangeValues[0]);
-      Notification.info("Retrieved latest data");
-    }).
-    error(function (data, status, headers, config) {
-      console.log('error');
-    });
-  };
+.controller('HomeController', function() {
 
-  $scope.reloadDashboardWithExchangeCurrency = function (exchange, value) {
-    $scope.selected.Exchange = exchange;
-    $scope.selected.Currency = value;
-    $rootScope.$emit('CurrencyChanged', $scope.selected);
-    
-  };
-
-  $scope.getDashboardData();
 
 });
