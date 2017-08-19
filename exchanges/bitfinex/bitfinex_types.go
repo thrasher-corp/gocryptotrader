36642e7410aeb73bfb1f60c5b0515ac707da98f2
Ryan O'Hara-Reid <oharareid.ryan@gmail.com>
2017-08-19 13:30:27 +1000

    Fixed linter issues, increased codecov, fixed general bugs for Bitfinex package

--- exchanges/bitfinex/bitfinex_types.go
+++ exchanges/bitfinex/bitfinex_types.go
@@ -1,50 +1,208 @@
 package bitfinex
 
-type BitfinexStats struct {
-	Period int64
-	Volume float64 `json:",string"`
+// Ticker holds basic ticker information from the exchange
+type Ticker struct {
+	Mid       float64 `json:"mid,string"`
+	Bid       float64 `json:"bid,string"`
+	Ask       float64 `json:"ask,string"`
+	Last      float64 `json:"last_price,string"`
+	Low       float64 `json:"low,string"`
+	High      float64 `json:"high,string"`
+	Volume    float64 `json:"volume,string"`
+	Timestamp string  `json:"timestamp"`
 }
 
-type BitfinexTicker struct {
-	Mid       float64 `json:",string"`
-	Bid       float64 `json:",string"`
-	Ask       float64 `json:",string"`
-	Last      float64 `json:"Last_price,string"`
-	Low       float64 `json:",string"`
-	High      float64 `json:",string"`
-	Volume    float64 `json:",string"`
-	Timestamp string
+// Stat holds individual statistics from exchange
+type Stat struct {
+	Period int64   `json:"period"`
+	Volume float64 `json:"volume,string"`
 }
 
-type BitfinexMarginLimits struct {
-	On_Pair           string
+// FundingBook holds current the full margin funding book
+type FundingBook struct {
+	Bids []Book `json:"bids"`
+	Asks []Book `json:"asks"`
+}
+
+// Orderbook holds orderbook information from bid and ask sides
+type Orderbook struct {
+	Bids []Book
+	Asks []Book
+}
+
+// TradeStructure holds executed trade information
+type TradeStructure struct {
+	Timestamp int64   `json:"timestamp"`
+	Tid       int64   `json:"tid"`
+	Price     float64 `json:"price,string"`
+	Amount    float64 `json:"amount,string"`
+	Exchange  string  `json:"exchange"`
+	Type      string  `json:"sell"`
+}
+
+// Lendbook holds most recent funding data for a relevent currency
+type Lendbook struct {
+	Bids []Book `json:"bids"`
+	Asks []Book `json:"asks"`
+}
+
+// Book is a generalised sub-type to hold book information
+type Book struct {
+	Price           float64 `json:"price,string"`
+	Rate            float64 `json:"rate,string"`
+	Amount          float64 `json:"amount,string"`
+	Period          int     `json:"period"`
+	Timestamp       string  `json:"timestamp"`
+	FlashReturnRate string  `json:"frr"`
+}
+
+// Lends holds the lent information by currency
+type Lends struct {
+	Rate       float64 `json:"rate,string"`
+	AmountLent float64 `json:"amount_lent,string"`
+	AmountUsed float64 `json:"amount_used,string"`
+	Timestamp  int64   `json:"timestamp"`
+}
+
+// SymbolDetails holds currency pair information
+type SymbolDetails struct {
+	Pair             string  `json:"pair"`
+	PricePrecision   int     `json:"price_precision"`
+	InitialMargin    float64 `json:"initial_margin,string"`
+	MinimumMargin    float64 `json:"minimum_margin,string"`
+	MaximumOrderSize float64 `json:"maximum_order_size,string"`
+	MinimumOrderSize float64 `json:"minimum_order_size,string"`
+	Expiration       string  `json:"expiration"`
+}
+
+// AccountInfo general account information with fees
+type AccountInfo struct {
+	MakerFees string `json:"maker_fees"`
+	TakerFees string `json:"taker_fees"`
+	Fees      []struct {
+		Pairs     string `json:"pairs"`
+		MakerFees string `json:"maker_fees"`
+		TakerFees string `json:"taker_fees"`
+	} `json:"fees"`
+}
+
+// AccountFees stores withdrawel account fee data from Bitfinex
+type AccountFees struct {
+	Withdraw struct {
+		BTC float64 `json:"BTC,string"`
+		LTC float64 `json:"LTC,string"`
+		ETH float64 `json:"ETH,string"`
+		ETC float64 `json:"ETC,string"`
+		ZEC float64 `json:"ZEC,string"`
+		XMR float64 `json:"XMR,string"`
+		DSH float64 `json:"DSH,string"`
+		XRP float64 `json:"XRP,string"`
+		IOT float64 `json:"IOT"`
+		EOS float64 `json:"EOS,string"`
+		SAN float64 `json:"SAN,string"`
+		OMG float64 `json:"OMG,string"`
+		BCH float64 `json:"BCH,string"`
+	} `json:"withdraw"`
+}
+
+// AccountSummary holds account summary data
+type AccountSummary struct {
+	TradeVolumePer30D []Currency `json:"trade_vol_30d"`
+	FundingProfit30D  []Currency `json:"funding_profit_30d"`
+	MakerFee          float64    `json:"maker_fee"`
+	TakerFee          float64    `json:"taker_fee"`
+}
+
+// Currency is a sub-type for AccountSummary data
+type Currency struct {
+	Currency string  `json:"curr"`
+	Volume   float64 `json:"vol,string"`
+	Amount   float64 `json:"amount,string"`
+}
+
+// DepositResponse holds deposit address information
+type DepositResponse struct {
+	Result   string `json:"string"`
+	Method   string `json:"method"`
+	Currency string `json:"currency"`
+	Address  string `json:"address"`
+}
+
+// KeyPermissions holds the key permissions for the API key set
+type KeyPermissions struct {
+	Account   Permission `json:"account"`
+	History   Permission `json:"history"`
+	Orders    Permission `json:"orders"`
+	Positions Permission `json:"positions"`
+	Funding   Permission `json:"funding"`
+	Wallets   Permission `json:"wallets"`
+	Withdraw  Permission `json:"withdraw"`
+}
+
+// Permission sub-type for KeyPermissions
+type Permission struct {
+	Read  bool `json:"read"`
+	Write bool `json:"write"`
+}
+
+// MarginInfo holds metadata for margin information from bitfinex
+type MarginInfo struct {
+	Info    MarginData
+	Message string `json:"message"`
+}
+
+// MarginData holds wallet information for margin trading
+type MarginData struct {
+	MarginBalance     float64        `json:"margin_balance,string"`
+	TradableBalance   float64        `json:"tradable_balance,string"`
+	UnrealizedPL      int64          `json:"unrealized_pl"`
+	UnrealizedSwap    int64          `json:"unrealized_swap"`
+	NetValue          float64        `json:"net_value,string"`
+	RequiredMargin    int64          `json:"required_margin"`
+	Leverage          float64        `json:"leverage,string"`
+	MarginRequirement float64        `json:"margin_requirement,string"`
+	MarginLimits      []MarginLimits `json:"margin_limits"`
+}
+
+// MarginLimits holds limit data per pair
+type MarginLimits struct {
+	OnPair            string  `json:"on_pair"`
 	InitialMargin     float64 `json:"initial_margin,string"`
 	MarginRequirement float64 `json:"margin_requirement,string"`
 	TradableBalance   float64 `json:"tradable_balance,string"`
 }
 
-type BitfinexMarginInfo struct {
-	MarginBalance     float64                `json:"margin_balance,string"`
-	TradableBalance   float64                `json:"tradable_balance,string"`
-	UnrealizedPL      int64                  `json:"unrealized_pl"`
-	UnrealizedSwap    int64                  `json:"unrealized_swap"`
-	NetValue          float64                `json:"net_value,string"`
-	RequiredMargin    int64                  `json:"required_margin"`
-	Leverage          float64                `json:"leverage,string"`
-	MarginRequirement float64                `json:"margin_requirement,string"`
-	MarginLimits      []BitfinexMarginLimits `json:"margin_limits"`
-	Message           string
+// Balance holds current balance data
+type Balance struct {
+	Type      string  `json:"type"`
+	Currency  string  `json:"currency"`
+	Amount    float64 `json:"amount,string"`
+	Available float64 `json:"available,string"`
 }
 
-type BitfinexOrder struct {
-	ID                    int64
-	Symbol                string
-	Exchange              string
+// WalletTransfer holds status of wallet to wallet content transfer on exchange
+type WalletTransfer struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
+// Withdrawal holds withdrawel status information
+type Withdrawal struct {
+	Status       string `json:"status"`
+	Message      string `json:"message"`
+	WithdrawalID int64  `json:"withdrawal_id,string"`
+}
+
+// Order holds order information when an order is in the market
+type Order struct {
+	ID                    int64   `json:"id"`
+	Symbol                string  `json:"symbol"`
+	Exchange              string  `json:"exchange"`
 	Price                 float64 `json:"price,string"`
 	AverageExecutionPrice float64 `json:"avg_execution_price,string"`
-	Side                  string
-	Type                  string
-	Timestamp             string
+	Side                  string  `json:"side"`
+	Type                  string  `json:"type"`
+	Timestamp             string  `json:"timestamp"`
 	IsLive                bool    `json:"is_live"`
 	IsCancelled           bool    `json:"is_cancelled"`
 	IsHidden              bool    `json:"is_hidden"`
@@ -55,7 +213,14 @@ type BitfinexOrder struct {
 	OrderID               int64   `json:"order_id"`
 }
 
-type BitfinexPlaceOrder struct {
+// OrderMultiResponse holds order information on the executed orders
+type OrderMultiResponse struct {
+	Orders []Order `json:"order_ids"`
+	Status string  `json:"status"`
+}
+
+// PlaceOrder is used for order placement
+type PlaceOrder struct {
 	Symbol   string  `json:"symbol"`
 	Amount   float64 `json:"amount,string"`
 	Price    float64 `json:"price,string"`
@@ -64,101 +229,13 @@ type BitfinexPlaceOrder struct {
 	Type     string  `json:"type"`
 }
 
-type BitfinexBalance struct {
-	Type      string
-	Currency  string
-	Amount    float64 `json:"amount,string"`
-	Available float64 `json:"available,string"`
+// GenericResponse holds the result for a generic response
+type GenericResponse struct {
+	Result string `json:"result"`
 }
 
-type BitfinexOffer struct {
-	ID              int64
-	Currency        string
-	Rate            float64 `json:"rate,string"`
-	Period          int64
-	Direction       string
-	Timestamp       string
-	Type            string
-	IsLive          bool    `json:"is_live"`
-	IsCancelled     bool    `json:"is_cancelled"`
-	OriginalAmount  float64 `json:"original_amount,string"`
-	RemainingAmount float64 `json:"remaining_amount,string"`
-	ExecutedAmount  float64 `json:"executed_amount,string"`
-}
-
-type BitfinexBookStructure struct {
-	Price, Amount, Timestamp string
-}
-
-type BitfinexFee struct {
-	Currency  string
-	TakerFees float64
-	MakerFees float64
-}
-
-type BitfinexOrderbook struct {
-	Bids []BitfinexBookStructure
-	Asks []BitfinexBookStructure
-}
-
-type BitfinexTradeStructure struct {
-	Timestamp, Tid                int64
-	Price, Amount, Exchange, Type string
-}
-
-type BitfinexSymbolDetails struct {
-	Pair             string  `json:"pair"`
-	PricePrecision   int     `json:"price_precision"`
-	InitialMargin    float64 `json:"initial_margin,string"`
-	MinimumMargin    float64 `json:"minimum_margin,string"`
-	MaximumOrderSize float64 `json:"maximum_order_size,string"`
-	MinimumOrderSize float64 `json:"minimum_order_size,string"`
-	Expiration       string  `json:"expiration"`
-}
-
-type BitfinexLends struct {
-	Rate       float64 `json:"rate,string"`
-	AmountLent float64 `json:"amount_lent,string"`
-	AmountUsed float64 `json:"amount_used,string"`
-	Timestamp  int64   `json:"timestamp"`
-}
-
-type BitfinexAccountInfo struct {
-	MakerFees string `json:"maker_fees"`
-	TakerFees string `json:"taker_fees"`
-	Fees      []struct {
-		Pairs     string `json:"pairs"`
-		MakerFees string `json:"maker_fees"`
-		TakerFees string `json:"taker_fees"`
-	} `json:"fees"`
-}
-
-type BitfinexDepositResponse struct {
-	Result   string `json:"string"`
-	Method   string `json:"method"`
-	Currency string `json:"currency"`
-	Address  string `json:"address"`
-}
-
-type BitfinexOrderMultiResponse struct {
-	Orders []BitfinexOrder `json:"order_ids"`
-	Status string          `json:"status"`
-}
-
-type BitfinexLendbookBidAsk struct {
-	Rate            float64 `json:"rate,string"`
-	Amount          float64 `json:"amount,string"`
-	Period          int     `json:"period"`
-	Timestamp       string  `json:"timestamp"`
-	FlashReturnRate string  `json:"frr"`
-}
-
-type BitfinexLendbook struct {
-	Bids []BitfinexLendbookBidAsk `json:"bids"`
-	Asks []BitfinexLendbookBidAsk `json:"asks"`
-}
-
-type BitfinexPosition struct {
+// Position holds position information
+type Position struct {
 	ID        int64   `json:"id"`
 	Symbol    string  `json:"string"`
 	Status    string  `json:"active"`
@@ -169,7 +246,8 @@ type BitfinexPosition struct {
 	PL        float64 `json:"pl,string"`
 }
 
-type BitfinexBalanceHistory struct {
+// BalanceHistory holds balance history information
+type BalanceHistory struct {
 	Currency    string  `json:"currency"`
 	Amount      float64 `json:"amount,string"`
 	Balance     float64 `json:"balance,string"`
@@ -177,18 +255,24 @@ type BitfinexBalanceHistory struct {
 	Timestamp   string  `json:"timestamp"`
 }
 
-type BitfinexMovementHistory struct {
-	ID          int64   `json:"id"`
-	Currency    string  `json:"currency"`
-	Method      string  `json:"method"`
-	Type        string  `json:"withdrawal"`
-	Amount      float64 `json:"amount,string"`
-	Description string  `json:"description"`
-	Status      string  `json:"status"`
-	Timestamp   string  `json:"timestamp"`
+// MovementHistory holds deposit and withdrawal history data
+type MovementHistory struct {
+	ID               int64   `json:"id"`
+	TxID             int64   `json:"txid"`
+	Currency         string  `json:"currency"`
+	Method           string  `json:"method"`
+	Type             string  `json:"withdrawal"`
+	Amount           float64 `json:"amount,string"`
+	Description      string  `json:"description"`
+	Address          string  `json:"address"`
+	Status           string  `json:"status"`
+	Timestamp        string  `json:"timestamp"`
+	TimestampCreated string  `json:"timestamp_created"`
+	Fee              float64 `json:"fee"`
 }
 
-type BitfinexTradeHistory struct {
+// TradeHistory holds trade history data
+type TradeHistory struct {
 	Price       float64 `json:"price,string"`
 	Amount      float64 `json:"amount,string"`
 	Timestamp   string  `json:"timestamp"`
@@ -200,7 +284,24 @@ type BitfinexTradeHistory struct {
 	OrderID     int64   `json:"order_id"`
 }
 
-type BitfinexMarginFunds struct {
+// Offer holds offer information
+type Offer struct {
+	ID              int64   `json:"id"`
+	Currency        string  `json:"currency"`
+	Rate            float64 `json:"rate,string"`
+	Period          int64   `json:"period"`
+	Direction       string  `json:"direction"`
+	Timestamp       string  `json:"timestamp"`
+	Type            string  `json:"type"`
+	IsLive          bool    `json:"is_live"`
+	IsCancelled     bool    `json:"is_cancelled"`
+	OriginalAmount  float64 `json:"original_amount,string"`
+	RemainingAmount float64 `json:"remaining_amount,string"`
+	ExecutedAmount  float64 `json:"executed_amount,string"`
+}
+
+// MarginFunds holds active funding information used in a margin positon
+type MarginFunds struct {
 	ID         int64   `json:"id"`
 	PositionID int64   `json:"position_id"`
 	Currency   string  `json:"currency"`
@@ -208,47 +309,46 @@ type BitfinexMarginFunds struct {
 	Period     int     `json:"period"`
 	Amount     float64 `json:"amount,string"`
 	Timestamp  string  `json:"timestamp"`
+	AutoClose  bool    `json:"auto_close"`
 }
 
-type BitfinexMarginTotalTakenFunds struct {
+// MarginTotalTakenFunds holds position funding including sum of active backing
+// as total swaps
+type MarginTotalTakenFunds struct {
 	PositionPair string  `json:"position_pair"`
 	TotalSwaps   float64 `json:"total_swaps,string"`
 }
 
-type BitfinexWalletTransfer struct {
-	Status  string `json:"status"`
-	Message string `json:"message"`
+// Fee holds fee data for a specified currency
+type Fee struct {
+	Currency  string
+	TakerFees float64
+	MakerFees float64
 }
 
-type BitfinexWithdrawal struct {
-	Status       string `json:"status"`
-	Message      string `json:"message"`
-	WithdrawalID int64  `json:"withdrawal_id"`
-}
-
-type BitfinexGenericResponse struct {
-	Result string `json:"result"`
-}
-
-type BitfinexWebsocketChanInfo struct {
+// WebsocketChanInfo holds websocket channel information
+type WebsocketChanInfo struct {
 	Channel string
 	Pair    string
 }
 
-type BitfinexWebsocketBook struct {
+// WebsocketBook holds booking information
+type WebsocketBook struct {
 	Price  float64
 	Count  int
 	Amount float64
 }
 
-type BitfinexWebsocketTrade struct {
+// WebsocketTrade holds trade information
+type WebsocketTrade struct {
 	ID        int64
 	Timestamp int64
 	Price     float64
 	Amount    float64
 }
 
-type BitfinexWebsocketTicker struct {
+// WebsocketTicker holds ticker information
+type WebsocketTicker struct {
 	Bid             float64
 	BidSize         float64
 	Ask             float64
@@ -259,7 +359,8 @@ type BitfinexWebsocketTicker struct {
 	Volume          float64
 }
 
-type BitfinexWebsocketPosition struct {
+// WebsocketPosition holds position information
+type WebsocketPosition struct {
 	Pair              string
 	Status            string
 	Amount            float64
@@ -268,14 +369,16 @@ type BitfinexWebsocketPosition struct {
 	MarginFundingType int
 }
 
-type BitfinexWebsocketWallet struct {
+// WebsocketWallet holds wallet information
+type WebsocketWallet struct {
 	Name              string
 	Currency          string
 	Balance           float64
 	UnsettledInterest float64
 }
 
-type BitfinexWebsocketOrder struct {
+// WebsocketOrder holds order data
+type WebsocketOrder struct {
 	OrderID    int64
 	Pair       string
 	Amount     float64
@@ -288,11 +391,17 @@ type BitfinexWebsocketOrder struct {
 	Notify     int
 }
 
-type BitfinexWebsocketTradeExecuted struct {
+// WebsocketTradeExecuted holds executed trade data
+type WebsocketTradeExecuted struct {
 	TradeID        int64
 	Pair           string
 	Timestamp      int64
 	OrderID        int64
 	AmountExecuted float64
 	PriceExecuted  float64
+}
+
+// ErrorCapture is a simple type for returned errors from Bitfinex
+type ErrorCapture struct {
+	Message string `json:"message"`
 }
