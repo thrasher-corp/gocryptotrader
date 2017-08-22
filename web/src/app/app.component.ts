9d4c901c467ba9405b3b90931a6596ed2fa0a642
GloriousCode <scott@gloriousedge.com>
2017-08-22 18:24:24 +1000

    Components and rearranging

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { ElectronService } from './providers/electron.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   constructor(public electronService: ElectronService) {
