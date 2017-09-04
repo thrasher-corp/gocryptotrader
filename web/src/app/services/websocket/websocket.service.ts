6e996e751daf97d7e07ba207bc61eee148db8690
GloriousCode <scott@gloriousedge.com>
2017-09-04 20:06:07 +1000

    Updates components to work with GCT

--- web/src/app/services/websocket/websocket.service.ts
+++ web/src/app/services/websocket/websocket.service.ts
@@ -17,12 +17,12 @@ export class WebsocketService {
 
   private create(url): Rx.Subject<MessageEvent> {
     let ws = new WebSocket(url);
-
+    
     let observable = Rx.Observable.create(
 	(obs: Rx.Observer<MessageEvent>) => {
 		ws.onmessage = obs.next.bind(obs);
 		ws.onerror = obs.error.bind(obs);
-		ws.onclose = obs.complete.bind(obs);
+    ws.onclose = obs.complete.bind(obs);
 		return ws.close.bind(ws);
 	})
 let observer = {
