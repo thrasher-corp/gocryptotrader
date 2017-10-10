29d9fd05f4be9d193af7412d215f45dbfe7f4bf6
GloriousCode <scott@gloriousedge.com>
2017-10-10 17:50:59 +1100

    Adds working sidebar. Has some funky height glitch that I can resolve later. Hard to get the viewport to be 100%

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
     {
         path: '',
-        component: DashboardComponent
+        component: HomeComponent
     },
     {
         path:'about',
