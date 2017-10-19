2969c6275945dbc1e25a244d118fcb9c12f5c5ac
GloriousCode <scott@gloriousedge.com>
2017-10-19 16:21:37 +1100

    Unceremoniously steals material.angular.io's theme picker. Very cool stuff from them!

---
+++ web/src/app/shared/theme-picker/theme-picker.ts
@@ -0,0 +1,74 @@
+import {Component, ViewEncapsulation, ChangeDetectionStrategy, NgModule} from '@angular/core';
+import { StyleManagerService } from './../../services/style-manager/style-manager.service';
+
+import { ThemeStorageService,DocsSiteTheme } from './../../services/theme-storage/theme-storage.service';
+import {CommonModule} from '@angular/common';
+
+
+@Component({
+  selector: 'theme-picker',
+  templateUrl: 'theme-picker.html',
+  styleUrls: ['theme-picker.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+  host: {'aria-hidden': 'true'},
+})
+export class ThemePickerComponent {
+  currentTheme;
+
+  themes = [
+    {
+      primary: '#673AB7',
+      accent: '#FFC107',
+      href: 'deeppurple-amber.css',
+      isDark: false,
+    },
+    {
+      primary: '#3F51B5',
+      accent: '#E91E63',
+      href: 'indigo-pink.css',
+      isDark: false,
+      isDefault: true,
+    },
+    {
+      primary: '#E91E63',
+      accent: '#607D8B',
+      href: 'pink-bluegrey.css',
+      isDark: true,
+    },
+    {
+      primary: '#9C27B0',
+      accent: '#4CAF50',
+      href: 'purple-green.css',
+      isDark: true,
+    },
+  ];
+
+  constructor(
+    public styleManager: StyleManagerService,
+    private _themeStorage: ThemeStorageService
+  ) {
+    const currentTheme = this._themeStorage.getStoredTheme();
+    if (currentTheme) {
+      this.installTheme(currentTheme);
+    }
+  }
+
+  installTheme(theme: DocsSiteTheme) {
+    this.currentTheme = this._getCurrentThemeFromHref(theme.href);
+
+    if (theme.isDefault) {
+      this.styleManager.removeStyle('theme');
+    } else {
+      this.styleManager.setStyle('theme', `assets/${theme.href}`);
+    }
+
+    if (this.currentTheme) {
+      this._themeStorage.storeTheme(this.currentTheme);
+    }
+  }
+
+  private _getCurrentThemeFromHref(href: string): DocsSiteTheme {
+    return this.themes.find(theme => theme.href === href);
+  }
+}
