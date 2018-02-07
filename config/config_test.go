5f10897b5bd40d0c97f23d5d6cf56de20e77e616
Adrian Gallagher <thrasher@addictionsoftware.com>
2018-02-07 13:11:57 +1100

    Bump number of exchanges supported

--- config/config_test.go
+++ config/config_test.go
@@ -89,7 +89,7 @@ func TestGetEnabledExchanges(t *testing.T) {
 	}
 
 	exchanges := cfg.GetEnabledExchanges()
-	if len(exchanges) != 24 {
+	if len(exchanges) != 25 {
 		t.Error(
 			"Test failed. TestGetEnabledExchanges. Enabled exchanges value mismatch",
 		)
@@ -141,7 +141,7 @@ func TestGetDisabledExchanges(t *testing.T) {
 }
 
 func TestCountEnabledExchanges(t *testing.T) {
-	defaultEnabledExchanges := 24
+	defaultEnabledExchanges := 25
 	GetConfigEnabledExchanges := GetConfig()
 	err := GetConfigEnabledExchanges.LoadConfig(ConfigTestFile)
 	if err != nil {
