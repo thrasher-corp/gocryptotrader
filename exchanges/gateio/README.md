5556515e3471b41498012d0c1e632d7b96678a4e
lion <lion.net@163.com>
2018-06-21 02:50:12 +0800

    增加 Gateio 交易所的支持，支持获取K线、支持的交易对、交易市场参数

---
+++ exchanges/gateio/README.md
@@ -0,0 +1,9 @@
+# GoCryptoTrader package gateio
+
+This gateio package is part of the GoCryptoTrader codebase.
+
+## Gateio Exchange 
+
+### Current Features
+
+特性可以看test文件
--- main.go
+++ main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"os"
-	"os/signal"
-	"runtime"
-	"strconv"
-	"syscall"
 	"time"
 
 	"github.com/thrasher-/gocryptotrader/communications"
 	"github.com/thrasher-/gocryptotrader/config"
 	"github.com/thrasher-/gocryptotrader/exchanges"
-	"github.com/thrasher-/gocryptotrader/exchanges/binance"
+	"github.com/thrasher-/gocryptotrader/exchanges/gateio"
 	"github.com/thrasher-/gocryptotrader/portfolio"
 )
 
@@ -44,7 +37,7 @@ var bot Bot
 // getDefaultConfig 获取默认配置
 func getDefaultConfig() config.ExchangeConfig {
 	return config.ExchangeConfig{
-		Name:                    "binance",
+		Name:                    "gateio",
 		Enabled:                 true,
 		Verbose:                 true,
 		Websocket:               false,
@@ -68,111 +61,24 @@ func getDefaultConfig() config.ExchangeConfig {
 		},
 		RequestCurrencyPairFormat: &config.CurrencyPairFormatConfig{
 			Uppercase: true,
+			Delimiter: "_",
 		},
-	}
-}
-
-// getExchangeInfo 获取交易所的规则和交易对信息
-func getExchangeInfo(b binance.Binance) {
-	info, err := b.GetExchangeInfo()
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		b, _ := json.Marshal(info)
-		fmt.Printf("%s \n", b)
-	}
-}
-
-// getOrderBook 获取交易深度
-func getOrderBook(b binance.Binance) {
-	model, err := b.GetOrderBook(b.GetSymbol(), 10)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("--------获取交易深度--------")
-		b, _ := json.Marshal(model)
-		fmt.Printf("%s \n", b)
-	}
-}
-
-// getRecentTrades 获取最近的交易记录
-func getRecentTrades(b binance.Binance) {
-	list, err := b.GetRecentTrades(b.GetSymbol(), 100)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("--------获取最近的交易记录--------")
-		for _, v := range list {
-			b, _ := json.Marshal(v)
-			fmt.Printf("%s \n", b)
-		}
-	}
-}
-
-// getCandleStickData 获取 k 线数据
-func getCandleStickData(b binance.Binance) {
-	list, err := b.GetCandleStickData(b.GetSymbol(), binance.BinanceIntervalFiveMinutes, 10)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("--------获取 k 线数据--------")
-		for _, v := range list {
-			b, _ := json.Marshal(v)
-			fmt.Printf("%s \n", b)
-		}
-	}
-}
-
-// getLatestSpotPrice 获取最新价格
-func getLatestSpotPrice(b binance.Binance) {
-	model, err := b.GetLatestSpotPrice(b.GetSymbol())
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("--------获取最新价格--------")
-		b, _ := json.Marshal(model)
-		fmt.Printf("%s \n", b)
-	}
-}
-
-// newOrder 新订单
-func newOrder(b binance.Binance) {
-	res, err := b.NewOrder(binance.NewOrderRequest{
-		Symbol:      b.GetSymbol(),
-		Side:        binance.BinanceRequestParamsSideSell,
-		TradeType:   binance.BinanceRequestParamsOrderLimit,
-		TimeInForce: binance.BinanceRequestParamsTimeGTC,
-		Quantity:    0.01,
-		Price:       1536.1,
-	})
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("--------下订单--------")
-		b, _ := json.Marshal(res)
-		fmt.Printf("%s \n", b)
-	}
-}
-
-// cancelOrder 取消订单
-func cancelOrder(b binance.Binance, orderID int64) {
-	res, err := b.CancelOrder(b.GetSymbol(), orderID, "")
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("--------取消订单--------")
-		b, _ := json.Marshal(res)
-		fmt.Printf("%s \n", b)
 	}
 }
 
 func main() {
 	fmt.Println(time.Now())
-	exchange := binance.Binance{}
+	exchange := gateio.Gateio{}
 	defaultConfig := getDefaultConfig()
 	exchange.SetDefaults()
 	fmt.Println("----------setup-------")
 	exchange.Setup(defaultConfig)
+
+	fmt.Println(exchange.GetKline(gateio.GateioKlinesRequestParams{
+		Symbol:   exchange.GetSymbol(),
+		GroupSec: gateio.GateioIntervalFiveMinutes,
+		HourSize: 1, //1小时内数据
+	}))
 
 	//获取交易所的规则和交易对信息
 	// getExchangeInfo(exchange)
@@ -195,164 +101,8 @@ func main() {
 	//取消订单
 	// cancelOrder(exchange, 82584683)
 
-	fmt.Println(exchange.GetAccount())
+	// fmt.Println(exchange.GetAccount())
 
 	// fmt.Println(exchange.GetSymbol())
 
-	// bot.shutdown = make(chan bool)
-	// HandleInterrupt()
-
-	// defaultPath, err := config.GetFilePath("")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// //Handle flags
-	// flag.StringVar(&bot.configFile, "config", defaultPath, "config file to load")
-	// dryrun := flag.Bool("dryrun", false, "dry runs bot, doesn't save config file")
-	// version := flag.Bool("version", false, "retrieves current GoCryptoTrader version")
-	// flag.Parse()
-
-	// if *version {
-	// 	fmt.Printf(BuildVersion(true))
-	// 	os.Exit(0)
-	// }
-
-	// if *dryrun {
-	// 	bot.dryRun = true
-	// }
-
-	// bot.config = &config.Cfg
-	// fmt.Println(banner)
-	// fmt.Println(BuildVersion(false))
-	// log.Printf("Loading config file %s..\n", bot.configFile)
-
-	// err = bot.config.LoadConfig(bot.configFile)
-	// if err != nil {
-	// 	log.Fatalf("Failed to load config. Err: %s", err)
-	// }
-
-	// AdjustGoMaxProcs()
-	// log.Printf("Bot '%s' started.\n", bot.config.Name)
-	// log.Printf("Bot dry run mode: %v.\n", common.IsEnabled(bot.dryRun))
-
-	// log.Printf("Available Exchanges: %d. Enabled Exchanges: %d.\n",
-	// 	len(bot.config.Exchanges),
-	// 	bot.config.CountEnabledExchanges())
-
-	// common.HTTPClient = common.NewHTTPClientWithTimeout(bot.config.GlobalHTTPTimeout)
-	// log.Printf("Global HTTP request timeout: %v.\n", common.HTTPClient.Timeout)
-
-	// SetupExchanges()
-	// if len(bot.exchanges) == 0 {
-	// 	log.Fatalf("No exchanges were able to be loaded. Exiting")
-	// }
-
-	// log.Println("Starting communication mediums..")
-	// bot.comms = communications.NewComm(bot.config.GetCommunicationsConfig())
-	// bot.comms.GetEnabledCommunicationMediums()
-
-	// log.Printf("Fiat display currency: %s.", bot.config.Currency.FiatDisplayCurrency)
-	// currency.BaseCurrency = bot.config.Currency.FiatDisplayCurrency
-	// currency.FXProviders = forexprovider.StartFXService(bot.config.GetCurrencyConfig().ForexProviders)
-	// log.Printf("Primary forex conversion provider: %s.\n", bot.config.GetPrimaryForexProvider())
-	// err = bot.config.RetrieveConfigCurrencyPairs(true)
-	// if err != nil {
-	// 	log.Fatalf("Failed to retrieve config currency pairs. Error: %s", err)
-	// }
-	// log.Println("Successfully retrieved config currencies.")
-	// log.Println("Fetching currency data from forex provider..")
-	// err = currency.SeedCurrencyData(common.JoinStrings(currency.FiatCurrencies, ","))
-	// if err != nil {
-	// 	log.Fatalf("Unable to fetch forex data. Error: %s", err)
-	// }
-
-	// bot.portfolio = &portfolio.Portfolio
-	// bot.portfolio.SeedPortfolio(bot.config.Portfolio)
-	// SeedExchangeAccountInfo(GetAllEnabledExchangeAccountInfo().Data)
-
-	// go portfolio.StartPortfolioWatcher()
-	// go TickerUpdaterRoutine()
-	// go OrderbookUpdaterRoutine()
-
-	// if bot.config.Webserver.Enabled {
-	// 	listenAddr := bot.config.Webserver.ListenAddress
-	// 	log.Printf(
-	// 		"HTTP Webserver support enabled. Listen URL: http://%s:%d/\n",
-	// 		common.ExtractHost(listenAddr), common.ExtractPort(listenAddr),
-	// 	)
-
-	// 	router := NewRouter(bot.exchanges)
-	// 	go func() {
-	// 		err = http.ListenAndServe(listenAddr, router)
-	// 		if err != nil {
-	// 			log.Fatal(err)
-	// 		}
-	// 	}()
-
-	// 	log.Println("HTTP Webserver started successfully.")
-	// 	log.Println("Starting websocket handler.")
-	// 	StartWebsocketHandler()
-	// } else {
-	// 	log.Println("HTTP RESTful Webserver support disabled.")
-	// }
-
-	// <-bot.shutdown
-	// Shutdown()
-}
-
-// AdjustGoMaxProcs adjusts the maximum processes that the CPU can handle.
-func AdjustGoMaxProcs() {
-	log.Println("Adjusting bot runtime performance..")
-	maxProcsEnv := os.Getenv("GOMAXPROCS")
-	maxProcs := runtime.NumCPU()
-	log.Println("Number of CPU's detected:", maxProcs)
-
-	if maxProcsEnv != "" {
-		log.Println("GOMAXPROCS env =", maxProcsEnv)
-		env, err := strconv.Atoi(maxProcsEnv)
-		if err != nil {
-			log.Println("Unable to convert GOMAXPROCS to int, using", maxProcs)
-		} else {
-			maxProcs = env
-		}
-	}
-	if i := runtime.GOMAXPROCS(maxProcs); i != maxProcs {
-		log.Fatal("Go Max Procs were not set correctly.")
-	}
-	log.Println("Set GOMAXPROCS to:", maxProcs)
-}
-
-// HandleInterrupt monitors and captures the SIGTERM in a new goroutine then
-// shuts down bot
-func HandleInterrupt() {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		sig := <-c
-		log.Printf("Captured %v, shutdown requested.", sig)
-		bot.shutdown <- true
-	}()
-}
-
-// Shutdown correctly shuts down bot saving configuration files
-func Shutdown() {
-	log.Println("Bot shutting down..")
-
-	if len(portfolio.Portfolio.Addresses) != 0 {
-		bot.config.Portfolio = portfolio.Portfolio
-	}
-
-	if !bot.dryRun {
-		err := bot.config.SaveConfig(bot.configFile)
-
-		if err != nil {
-			log.Println("Unable to save config.")
-		} else {
-			log.Println("Config file saved successfully.")
-		}
-	}
-
-	log.Println("Exiting.")
-	os.Exit(0)
 }
