ff5c1eb62b9f57f6b4600eb3ef5afc5261c69d2a
Ermal Guni <ermalguni@gmail.com>
2018-08-05 15:14:24 +0200

    added okex websocket types

--- exchanges/okex/okex_types.go
+++ exchanges/okex/okex_types.go
@@ -1,5 +1,7 @@
 package okex
 
+import "encoding/json"
+
 // ContractPrice holds date and ticker price price for contracts.
 type ContractPrice struct {
 	Date   string `json:"date"`
@@ -15,6 +17,33 @@ type ContractPrice struct {
 	} `json:"ticker"`
 	Result bool        `json:"result"`
 	Error  interface{} `json:"error_code"`
+}
+
+type MultiStreamData struct {
+	Channel string          `json:"channel"`
+	Data    json.RawMessage `json:"data"`
+}
+
+type TickerStreamData struct {
+	Buy       string  `json:"buy"`
+	Change    string  `json:"change"`
+	High      string  `json:"high"`
+	Low       string  `json:"low"`
+	Last      string  `json:"last"`
+	Sell      string  `json:"sell"`
+	DayLow    string  `json:"dayLow"`
+	DayHigh   string  `json:"dayHigh"`
+	Timestamp float64 `json:"timestamp"`
+	Vol       string  `json:"vol"`
+}
+
+type DealsStreamData = [][]string
+type KlineStreamData = [][]string
+
+type DepthStreamData struct {
+	Asks      [][]string `json:"asks"`
+	Bids      [][]string `json:"bids"`
+	Timestamp float64    `json:"timestamp"`
 }
 
 // ContractDepth response depth
