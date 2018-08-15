d80b2c96b2d0f92cb1f47d0ba083aa04dab7b4ea
GloriousCode <scott@gloriousedge.com>
2018-08-16 08:44:32 +1000

    npm audit run Removes npx

--- web/src/polyfills.ts
+++ web/src/polyfills.ts
@@ -30,7 +30,7 @@
 // import 'core-js/es6/date';
 // import 'core-js/es6/array';
 // import 'core-js/es6/regexp';
-// import 'core-js/es6/map';
+ import 'core-js/es6/map';
 // import 'core-js/es6/set';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
