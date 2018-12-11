09560095cdb403e1a7519781854d0a1630de1eee
Scott Grant <scott.grant@thrasher.io>
2018-12-12 08:55:26 +1100

    Fixes OKEX delimiter

--- exchanges/okex/okex.go
+++ exchanges/okex/okex.go
@@ -107,9 +107,9 @@ func (o *OKEX) SetDefaults() {
 	o.Verbose = false
 	o.RESTPollingDelay = 10
 	o.APIWithdrawPermissions = exchange.AutoWithdrawCrypto
-	o.RequestCurrencyPairFormat.Delimiter = "-"
-	o.RequestCurrencyPairFormat.Uppercase = true
-	o.ConfigCurrencyPairFormat.Delimiter = "-"
+	o.RequestCurrencyPairFormat.Delimiter = "_"
+	o.RequestCurrencyPairFormat.Uppercase = false
+	o.ConfigCurrencyPairFormat.Delimiter = "_"
 	o.ConfigCurrencyPairFormat.Uppercase = true
 	o.SupportsAutoPairUpdating = true
 	o.SupportsRESTTickerBatching = false
