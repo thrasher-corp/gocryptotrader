635f0f1e61362ac6eefbc69c12e6dac5d440c2cb
Shazbert <oharareid.ryan@gmail.com>
2019-01-14 16:21:30 +1100

    Remove support for deposit address on ANX exchange

--- exchanges/anx/anx_wrapper.go
+++ exchanges/anx/anx_wrapper.go
@@ -306,7 +306,7 @@ func (a *ANX) GetOrderInfo(orderID int64) (exchange.OrderDetail, error) {
 
 // GetDepositAddress returns a deposit address for a specified currency
 func (a *ANX) GetDepositAddress(cryptocurrency pair.CurrencyItem) (string, error) {
-	return "", common.ErrNotYetImplemented
+	return "", common.ErrFunctionNotSupported
 }
 
 // WithdrawCryptocurrencyFunds returns a withdrawal ID when a withdrawal is
