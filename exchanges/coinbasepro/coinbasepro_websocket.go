thrasher-corp/gocryptotrader

commit dc236c251ec65b32685ddb5e9de35a4a964d7e04

Optimisation: large structs/huge param fixes (part 2) (#262)
* updated golangci config to enable hugeparam linter

* ModifyOrder struct usage converted to a pointer

* OrderBook conversion to struct

* More conversion of large structs to pointers

* updated golangci config to enable hugeparam linter

* ModifyOrder struct usage converted to a pointer

* OrderBook conversion to struct

* More conversion of large structs to pointers

* disabled hugeParam check for golang again

* changed based on suggested feedback and fix for no default provider

* fixed typing

exchanges/coinbasepro/coinbasepro_websocket.go
@@ -173,7 +173,7 @@ func (c *CoinbasePro) WsHandleData() {
 					continue
 				}
 
-				err = c.ProcessSnapshot(snapshot)
+				err = c.ProcessSnapshot(&snapshot)
 				if err != nil {
 					c.Websocket.DataHandler <- err
 					continue
@@ -198,8 +198,8 @@ func (c *CoinbasePro) WsHandleData() {
 }
 
 // ProcessSnapshot processes the initial orderbook snap shot
-func (c *CoinbasePro) ProcessSnapshot(snapshot WebsocketOrderbookSnapshot) error {
-	var base orderbook.Base
+func (c *CoinbasePro) ProcessSnapshot(snapshot *WebsocketOrderbookSnapshot) error {
+	var base *orderbook.Base
 	for _, bid := range snapshot.Bids {
 		price, err := strconv.ParseFloat(bid[0].(string), 64)
 		if err != nil {

commit a0e291097e1806647e80bcfefeaba3a346fb83a6

Fix CoinbasePro websocket bug introduced in PR #262 (#269)

exchanges/coinbasepro/coinbasepro_websocket.go
@@ -199,7 +199,7 @@ func (c *CoinbasePro) WsHandleData() {
 
 // ProcessSnapshot processes the initial orderbook snap shot
 func (c *CoinbasePro) ProcessSnapshot(snapshot *WebsocketOrderbookSnapshot) error {
-	var base *orderbook.Base
+	var base orderbook.Base
 	for _, bid := range snapshot.Bids {
 		price, err := strconv.ParseFloat(bid[0].(string), 64)
 		if err != nil {
@@ -234,7 +234,7 @@ func (c *CoinbasePro) ProcessSnapshot(snapshot *WebsocketOrderbookSnapshot) erro
 	base.AssetType = "SPOT"
 	base.Pair = pair
 
-	err := c.Websocket.Orderbook.LoadSnapshot(base, c.GetName(), false)
+	err := c.Websocket.Orderbook.LoadSnapshot(&base, c.GetName(), false)
 	if err != nil {
 		return err
 	}