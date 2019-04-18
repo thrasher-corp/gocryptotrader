4748227a10af6e74d0db725900e295ab72da0ad8
Vadim Zhuk <vadimzhukck@gmail.com>
2019-04-18 16:54:38 +0300

    - poloniex: fix 422 on private API call

--- exchanges/poloniex/poloniex.go
+++ exchanges/poloniex/poloniex.go
@@ -870,7 +870,7 @@ func (p *Poloniex) SendAuthenticatedHTTPRequest(method, endpoint string, values 
 	headers["Key"] = p.APIKey
 
 	if p.Nonce.Get() == 0 {
-		p.Nonce.Set(time.Now().UnixNano())
+		p.Nonce.Set(time.Now().UnixNano() * 2) // TODO: this weird fixing 422 error (wrong nonce). if who find a better way -- please fix this spike
 	} else {
 		p.Nonce.Inc()
 	}
