ec13bc9972a39c978de7e18413214815e9e1c370
shazbert <oharareid.ryan@gmail.com>
2019-05-27 13:37:42 +1000

    Add continue so we dont append zero val

--- exchanges/btcmarkets/btcmarkets_wrapper.go
+++ exchanges/btcmarkets/btcmarkets_wrapper.go
@@ -232,6 +232,7 @@ func (b *BTCMarkets) CancelAllOrders(_ *exchange.OrderCancellation) (exchange.Ca
 		orderIDInt, err := strconv.ParseInt(openOrders[i].ID, 10, 64)
 		if err != nil {
 			cancelAllOrdersResponse.OrderStatus[openOrders[i].ID] = err.Error()
+			continue
 		}
 		orderList = append(orderList, orderIDInt)
 	}
