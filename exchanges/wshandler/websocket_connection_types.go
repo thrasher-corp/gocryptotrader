d20ce1ea32ed4000266d5816ac69fa1a0255ac4a
Scott Grant <scott.grant@thrasher.io>
2019-07-22 11:42:18 +1000

    Removes unused interface

--- exchanges/wshandler/websocket_connection_types.go
+++ exchanges/wshandler/websocket_connection_types.go
@@ -1,23 +1,11 @@
 package wshandler
 
 import (
-	"net/http"
 	"sync"
 	"time"
 
 	"github.com/gorilla/websocket"
 )
-
-// IWebsocketConnection an interface for testing
-type IWebsocketConnection interface {
-	AddResponseWithID(id int64, data []byte)
-	Dial(dialer *websocket.Dialer, headers http.Header) error
-	SendMessage(data interface{}) error
-	SendMessageReturnResponse(id int64, request interface{}) ([]byte, error)
-	WaitForResult(id int64, wg *sync.WaitGroup)
-	ReadMessage() (WebsocketResponse, error)
-	GenerateMessageID(useNano bool) int64
-}
 
 // WebsocketConnection contains all the datas needed to send a message to a WS
 type WebsocketConnection struct {
