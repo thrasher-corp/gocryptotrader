07f8384832c125aebdaea6e667ff994b8e97a0e4
Andrew Jackson <andrew@disvelop.net>
2019-10-01 09:17:12 +1000

    typos and check against goose error instead of string :D

--- database/models/postgres/psql_main_test.go
+++ database/models/postgres/psql_main_test.go
@@ -233,7 +233,7 @@ func (p *pgTester) conn() (*sql.DB, error) {
 	path := filepath.Join("..", "..", "migrations")
 	err = goose.Run("up", p.dbConn, "postgres", path, "")
 	if err != nil {
-		if err.Error() == "no next version found" {
+		if err == goose.ErrNoNextVersion {
 			return p.dbConn, nil
 		}
 		return nil, err
