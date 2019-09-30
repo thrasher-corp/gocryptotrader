709431b6be93e8e24d39c7eb20458ff9211ec8ac
Andrew Jackson <andrew@disvelop.net>
2019-10-01 09:08:14 +1000

    added error check for migration

--- database/models/postgres/psql_main_test.go
+++ database/models/postgres/psql_main_test.go
@@ -233,6 +233,9 @@ func (p *pgTester) conn() (*sql.DB, error) {
 	path := filepath.Join("..", "..", "migrations")
 	err = goose.Run("up", p.dbConn, "postgres", path, "")
 	if err != nil {
+		if err.Error() == "no next version found" {
+			return p.dbConn, nil
+		}
 		return nil, err
 	}
 
