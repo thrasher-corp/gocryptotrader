0947343e6c4f2abc36e7d2e520dbd640f46ce92c
Scott Grant <scott.grant@thrasher.io>
2019-10-21 16:26:11 +1100

    imports and casing

--- exchanges/exchange.go
+++ exchanges/exchange.go
@@ -8,11 +8,11 @@ import (
 	"strings"
 	"time"
 
-	"github.com/thrasher-corp/gocryptotrader/exchanges/protocol"
 	"github.com/thrasher-corp/gocryptotrader/common/crypto"
 	"github.com/thrasher-corp/gocryptotrader/config"
 	"github.com/thrasher-corp/gocryptotrader/currency"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/asset"
+	"github.com/thrasher-corp/gocryptotrader/exchanges/protocol"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/request"
 	log "github.com/thrasher-corp/gocryptotrader/logger"
 )
