194a2dbd5501dfd830089be3afd1491bc2b9a884
Shazbert <oharareid.ryan@gmail.com>
2019-12-02 13:30:44 +1100

    fix go import issue

--- exchanges/huobi/huobi_websocket.go
+++ exchanges/huobi/huobi_websocket.go
@@ -9,11 +9,11 @@ import (
 	"strings"
 	"time"
 
-	"github.com/thrasher-corp/gocryptotrader/common/crypto"
-	"github.com/thrasher-corp/gocryptotrader/exchanges/asset"
 	"github.com/gorilla/websocket"
+	"github.com/thrasher-corp/gocryptotrader/common/crypto"
 	"github.com/thrasher-corp/gocryptotrader/currency"
 	exchange "github.com/thrasher-corp/gocryptotrader/exchanges"
+	"github.com/thrasher-corp/gocryptotrader/exchanges/asset"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/orderbook"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/websocket/wshandler"
 	log "github.com/thrasher-corp/gocryptotrader/logger"
