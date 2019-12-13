e5e490962714268048e50700a44f5477522b2dc9
Andrew Jackson <andrew@disvelop.net>
2019-12-13 13:47:10 +1100

    Increased test cases for NTPClient

--- ntpclient/ntpclient_test.go
+++ ntpclient/ntpclient_test.go
@@ -13,4 +13,14 @@ func TestNTPClient(t *testing.T) {
 	if reflect.TypeOf(v) != reflect.TypeOf(time.Time{}) {
 		t.Errorf("NTPClient should return time.Time{}")
 	}
+
+	if v.IsZero() {
+		t.Error("NTPClient should return valid time received zero value")
+	}
+
+	const timeFormat = "2006-01-02T15:04:05"
+
+	if v.UTC().Format(timeFormat) != time.Now().UTC().Format(timeFormat) {
+		t.Errorf("NTPClient returned incorrect time received: %v", v.UTC().Format(timeFormat))
+	}
 }
