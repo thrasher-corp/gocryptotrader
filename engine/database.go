cdb0149d5565600a8b1a5681171a166d63063d67
Adrian Gallagher <adrian.gallagher@thrasher.io>
2020-01-13 16:46:40 +1100

    Use atomic.LoadInt32/AddInt32 for database subsystem

--- engine/database.go
+++ engine/database.go
@@ -18,18 +18,25 @@ var (
 )
 
 type databaseManager struct {
-	running  atomic.Value
+	started  int32
+	stopped  int32
 	shutdown chan struct{}
 }
 
 func (a *databaseManager) Started() bool {
-	return a.running.Load() == true
+	return atomic.LoadInt32(&a.started) == 1
 }
 
 func (a *databaseManager) Start() (err error) {
-	if a.Started() {
+	if atomic.AddInt32(&a.started, 1) != 1 {
 		return errors.New("database manager already started")
 	}
+
+	defer func() {
+		if err != nil {
+			atomic.CompareAndSwapInt32(&a.started, 1, 0)
+		}
+	}()
 
 	log.Debugln(log.DatabaseMgr, "Database manager starting...")
 
@@ -70,11 +77,13 @@ func (a *databaseManager) Start() (err error) {
 }
 
 func (a *databaseManager) Stop() error {
-	if !a.Started() {
-		return errors.New("database manager already stopped")
+	if atomic.LoadInt32(&a.started) == 0 {
+		return errors.New("database manager not started")
 	}
 
-	log.Debugln(log.DatabaseMgr, "Database manager shutting down...")
+	if atomic.AddInt32(&a.stopped, 1) != 1 {
+		return errors.New("database manager is already stopping")
+	}
 
 	err := dbConn.SQL.Close()
 	if err != nil {
@@ -91,11 +100,10 @@ func (a *databaseManager) run() {
 
 	t := time.NewTicker(time.Second * 2)
 
-	a.running.Store(true)
-
 	defer func() {
 		t.Stop()
-		a.running.Store(false)
+		atomic.CompareAndSwapInt32(&a.stopped, 1, 0)
+		atomic.CompareAndSwapInt32(&a.started, 1, 0)
 
 		Bot.ServicesWG.Done()
 
