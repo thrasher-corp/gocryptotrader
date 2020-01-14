a914fd341617dfcbab981bb6a7c0474e375bd9cd
Andrew Jackson <andrew@disvelop.net>
2020-01-14 11:58:05 +1100

    lower time on ntp client to stop slippage

--- ntpclient/ntpclient_test.go
+++ ntpclient/ntpclient_test.go
@@ -18,7 +18,7 @@ func TestNTPClient(t *testing.T) {
 		t.Error("NTPClient should return valid time received zero value")
 	}
 
-	const timeFormat = "2006-01-02T15:04:05"
+	const timeFormat = "2006-01-02T15:04"
 
 	if v.UTC().Format(timeFormat) != time.Now().UTC().Format(timeFormat) {
 		t.Errorf("NTPClient returned incorrect time received: %v", v.UTC().Format(timeFormat))
