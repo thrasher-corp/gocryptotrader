7f267dee680330e79501a9ef1ebd9e6bb82ad2ac
Andrew Jackson <andrew@disvelop.net>
2020-01-17 13:10:34 +1100

    Removed unused vars

--- gctscript/wrappers/validator/validator_types.go
+++ gctscript/wrappers/validator/validator_types.go
@@ -13,27 +13,9 @@ var (
 	// IsTestExecution if test is executed under test conditions
 	IsTestExecution bool
 
-	exch = &objects.String{
-		Value: "BTC Markets",
-	}
 	exchError = &objects.String{
 		Value: "error",
 	}
-	currencyPair = &objects.String{
-		Value: "BTC-AUD",
-	}
-	delimiter = &objects.String{
-		Value: "-",
-	}
-	assetType = &objects.String{
-		Value: "SPOT",
-	}
-	orderID = &objects.String{
-		Value: "1235",
-	}
-
-	tv            = objects.TrueValue
-	fv            = objects.FalseValue
 	errTestFailed = errors.New("test failed")
 )
 
