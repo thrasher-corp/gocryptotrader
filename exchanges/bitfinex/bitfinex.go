446de67723432ca4d7b3c7e8ed4cf8b5624d8b10
Andrew Jackson <andrew@disvelop.net>
2020-02-13 07:44:35 +1100

    reordered imports

--- exchanges/bitfinex/bitfinex.go
+++ exchanges/bitfinex/bitfinex.go
@@ -17,8 +17,8 @@ import (
 	"github.com/thrasher-corp/gocryptotrader/exchanges/order"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/request"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/websocket/wshandler"
-	"github.com/thrasher-corp/gocryptotrader/portfolio/withdraw"
 	"github.com/thrasher-corp/gocryptotrader/log"
+	"github.com/thrasher-corp/gocryptotrader/portfolio/withdraw"
 )
 
 const (
