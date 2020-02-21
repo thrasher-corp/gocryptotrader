256adf7a3c9ef60dfd37aa6e0b601b691418099b
Andrew Jackson <andrew@disvelop.net>
2020-02-21 11:29:32 +1100

    removed comment

--- database/testhelpers/test_helpers.go
+++ database/testhelpers/test_helpers.go
@@ -26,7 +26,7 @@ func GetConnectionDetails() *database.Config {
 			Driver:  "postgres",
 			ConnectionDetails: drivers.ConnectionDetails{
 				Host:     "localhost",
-				Port:     5432,<exchange> <currency>  <amount> <address> <addresstag> <fee> <description>
+				Port:     5432,
 				Username: "postgres",
 				Password: "",
 				Database: "gct_dev_ci",
