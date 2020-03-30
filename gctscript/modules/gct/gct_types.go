fe6fe7138df608b18608e4933b6a2cc644488152
Andrew Jackson <andrew@disvelop.net>
2020-03-30 13:58:15 +1100

    reverted rpc.pb.go to master

--- gctscript/modules/gct/gct_types.go
+++ gctscript/modules/gct/gct_types.go
@@ -12,7 +12,7 @@ const (
 )
 
 var errInvalidInterval = errors.New("invalid interval")
-var supportedDurations = []string{"1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "6h", "12h", "24h", "1d", "3d", "1w"}
+var supportedDurations = []string{"1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "6h", "12h", "24h", "1d", "3d", "1w", "4w"}
 
 // Modules map of all loadable modules
 var Modules = map[string]map[string]tengo.Object{
