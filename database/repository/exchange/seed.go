56218ab74f31657e8da04932249036813ee9a215
Andrew Jackson <andrew@disvelop.net>
2020-07-16 16:39:22 +1000

    WIP

--- database/repository/exchange/seed.go
+++ database/repository/exchange/seed.go
@@ -1,95 +1,12 @@
 package exchange
 
-var (
-	allExchanges = []Details{
-		{
-			Name: "Alphapoint",
-		},
-		{
-			Name: "Binance",
-		},
-		{
-			Name: "Bitfinex",
-		},
-		{
-			Name: "Bitflyer",
-		},
-		{
-			Name: "Bithumb",
-		},
-		{
-			Name: "Bitmex",
-		},
-		{
-			Name: "Bitstamp",
-		},
-		{
-			Name: "Bittrex",
-		},
-		{
-			Name: "BTC Markets",
-		},
-		{
-			Name: "BTSE",
-		},
-		{
-			Name: "Coinbase Pro",
-		},
-		{
-			Name: "Coinbene",
-		},
-		{
-			Name: "Coinut",
-		},
-		{
-			Name: "Exmo",
-		},
-		{
-			Name: "GateIO",
-		},
-		{
-			Name: "Gemini",
-		},
-		{
-			Name: "HitBTC",
-		},
-		{
-			Name: "Huobi",
-		},
-		{
-			Name: "itBit",
-		},
-		{
-			Name: "Kraken",
-		},
-		{
-			Name: "lakeBTC",
-		},
-		{
-			Name: "lBank",
-		},
-		{
-			Name: "Local Bitcoins",
-		},
-		{
-			Name: "OKCoin",
-		},
-		{
-			Name: "OKEX",
-		},
-		{
-			Name: "Poloniex",
-		},
-		{
-			Name: "YoBit",
-		},
-		{
-			Name: "ZB",
-		},
-	}
-)
-
 // Seed will import seeded data to the database
 func Seed() error {
+	var allExchanges []Details
+	// for x := range exchange.Exchanges {
+	// 	allExchanges = append(allExchanges, Details{
+	// 		Name: exchange.Exchanges[x],
+	// 	})
+	// }
 	return InsertMany(allExchanges)
 }
--- database/seed/seed.go
+++ database/seed/seed.go
@@ -1,12 +1,22 @@
 package seed
 
-import "github.com/thrasher-corp/gocryptotrader/database/repository/exchange"
+import (
+	exchangeDB "github.com/thrasher-corp/gocryptotrader/database/repository/exchange"
+	exchange "github.com/thrasher-corp/gocryptotrader/exchanges"
+)
 
-func setupEngine() {
 
+func Exchange() error {
+	var allExchanges []exchangeDB.Details
+	for x := range exchange.Exchanges {
+		allExchanges = append(allExchanges, exchangeDB.Details{
+			Name: exchange.Exchanges[x],
+		})
+	}
+	return exchangeDB.InsertMany(allExchanges)
 }
 
 // Run executes all seeding methods for database
 func Run() error {
-	return exchange.Seed()
+	return Exchange()
 }
