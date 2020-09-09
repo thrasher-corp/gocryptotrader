5e709a37b13fd7e51d45e639250c9d490fc52320
Scott <scott.grant@thrasher.io>
2020-09-09 13:03:27 +1000

    Fixes asset typo and changes postgres migration to work

--- database/migrations/20200814121412_update_candle_add_not_null_to_asset/postgres.sql
+++ database/migrations/20200814121412_update_candle_add_not_null_to_asset/postgres.sql
@@ -4,4 +4,4 @@ ALTER TABLE candle ALTER COLUMN asset SET NOT NULL;
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
-ALTER TABLE candle ALTER COLUMN aseet SET NULL;
+ALTER TABLE candle ALTER COLUMN asset DROP NOT NULL;
