35d965536c36b8079a26aeba1b7d3cbbf8cf3504
Rauno Ots <rots00@gmail.com>
2020-09-10 21:21:09 +0200

    remove parallel for loadConfigWithSettings

--- engine/engine_test.go
+++ engine/engine_test.go
@@ -53,7 +53,6 @@ func Test_loadConfigWithSettings(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
 			// prepare the 'flags'
 			flagSet = make(map[string]bool)
 			for _, v := range tt.flags {
