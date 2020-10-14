f55307a52c301877245d453f7be2fbd701c00acb
Vazha Bezhanishvili <vazha.bezhanishvili@elegro.eu>
2020-10-14 14:58:14 +0300

    refactored RoundFloat func in common/math package

--- common/math/math.go
+++ common/math/math.go
@@ -31,21 +31,6 @@ func CalculateNetProfit(amount, priceThen, priceNow, costs float64) float64 {
 
 // RoundFloat rounds your floating point number to the desired decimal place
 func RoundFloat(x float64, prec int) float64 {
-	var rounder float64
 	pow := math.Pow(10, float64(prec))
-	intermed := x * pow
-	_, frac := math.Modf(intermed)
-	intermed += .5
-	x = .5
-	if frac < 0.0 {
-		x = -.5
-		intermed--
-	}
-	if frac >= x {
-		rounder = math.Ceil(intermed)
-	} else {
-		rounder = math.Floor(intermed)
-	}
-
-	return rounder / pow
+	return math.Round(x*pow) / pow
 }
