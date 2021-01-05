608cb7d5eec842e58af3eec98f647e3d756def0f
gloriousCode <scott.grant@thrasher.io>
2021-01-05 16:33:53 +1100

    Fixes test

--- backtester/eventtypes/kline/kline_test.go
+++ backtester/eventtypes/kline/kline_test.go
@@ -1,17 +1,23 @@
 package kline
 
+import (
+	"testing"
+
+	"github.com/thrasher-corp/gocryptotrader/backtester/data"
+)
+
 func TestDataType(t *testing.T) {
-k := Kline{}
-if k.DataType() != data.CandleType {
-t.Error("expected candletype")
-}
+	k := Kline{}
+	if k.DataType() != data.CandleType {
+		t.Error("expected candletype")
+	}
 }
 
 func TestClose(t *testing.T) {
-k := Kline{
-Close: 1337,
-}
-if k.Price() != 1337 {
-t.Error("expected 1337")
-}
+	k := Kline{
+		Close: 1337,
+	}
+	if k.Price() != 1337 {
+		t.Error("expected 1337")
+	}
 }
