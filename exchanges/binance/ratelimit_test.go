0520f6beaad4aa0b952d685e24fcedd9122f5e3c
Scott <gloriousCode@users.noreply.github.com>
2021-03-04 14:58:30 +1100

    Improvement: Binance Futures rate limits (#635)
    * Implements rate limits for Binance futures
    
    * Updates rate limit
    
    * Fixes bad merge getorder
    
    * Creates getKlineRateBudget function. Adds missing limit cases to switch
    
    * Adds additional rate limit details
    
    * receive THIS window

--- exchanges/binance/ratelimit_test.go
+++ exchanges/binance/ratelimit_test.go
@@ -13,20 +13,17 @@ func TestRateLimit_Limit(t *testing.T) {
 		Expected request.EndpointLimit
 		Limit    request.EndpointLimit
 	}{
-		"All Orderbooks Ticker": {Expected: limitOrderbookTickerAll, Limit: bestPriceLimit("")},
-		"Orderbook Ticker":      {Expected: limitDefault, Limit: bestPriceLimit(symbol)},
-		"All Open Orders":       {Expected: limitOpenOrdersAll, Limit: openOrdersLimit("")},
-		"Open Orders":           {Expected: limitOrder, Limit: openOrdersLimit(symbol)},
-		"Orderbook Depth 5":     {Expected: limitDefault, Limit: orderbookLimit(5)},
-		"Orderbook Depth 10":    {Expected: limitDefault, Limit: orderbookLimit(10)},
-		"Orderbook Depth 20":    {Expected: limitDefault, Limit: orderbookLimit(20)},
-		"Orderbook Depth 50":    {Expected: limitDefault, Limit: orderbookLimit(50)},
-		"Orderbook Depth 100":   {Expected: limitDefault, Limit: orderbookLimit(100)},
-		"Orderbook Depth 500":   {Expected: limitOrderbookDepth500, Limit: orderbookLimit(500)},
-		"Orderbook Depth 1000":  {Expected: limitOrderbookDepth1000, Limit: orderbookLimit(1000)},
-		"Orderbook Depth 5000":  {Expected: limitOrderbookDepth5000, Limit: orderbookLimit(5000)},
-		"All Symbol Prices":     {Expected: limitSymbolPriceAll, Limit: symbolPriceLimit("")},
-		"Symbol Price":          {Expected: limitDefault, Limit: symbolPriceLimit(symbol)},
+		"All Orderbooks Ticker": {Expected: spotOrderbookTickerAllRate, Limit: bestPriceLimit("")},
+		"Orderbook Ticker":      {Expected: spotDefaultRate, Limit: bestPriceLimit(symbol)},
+		"Open Orders":           {Expected: spotOrderRate, Limit: openOrdersLimit(symbol)},
+		"Orderbook Depth 5":     {Expected: spotDefaultRate, Limit: orderbookLimit(5)},
+		"Orderbook Depth 10":    {Expected: spotDefaultRate, Limit: orderbookLimit(10)},
+		"Orderbook Depth 20":    {Expected: spotDefaultRate, Limit: orderbookLimit(20)},
+		"Orderbook Depth 50":    {Expected: spotDefaultRate, Limit: orderbookLimit(50)},
+		"Orderbook Depth 100":   {Expected: spotDefaultRate, Limit: orderbookLimit(100)},
+		"Orderbook Depth 500":   {Expected: spotOrderbookDepth500Rate, Limit: orderbookLimit(500)},
+		"Orderbook Depth 1000":  {Expected: spotOrderbookDepth1000Rate, Limit: orderbookLimit(1000)},
+		"Orderbook Depth 5000":  {Expected: spotOrderbookDepth5000Rate, Limit: orderbookLimit(5000)},
 	}
 	for name, tt := range testTable {
 		tt := tt
@@ -48,10 +45,10 @@ func TestRateLimit_Limit(t *testing.T) {
 
 func TestRateLimit_LimitStatic(t *testing.T) {
 	testTable := map[string]request.EndpointLimit{
-		"Default":           limitDefault,
-		"Historical Trades": limitHistoricalTrades,
-		"All Price Changes": limitPriceChangeAll,
-		"All Orders":        limitOrdersAll,
+		"Default":           spotDefaultRate,
+		"Historical Trades": spotHistoricalTradesRate,
+		"All Price Changes": spotPriceChangeAllRate,
+		"All Orders":        spotOrdersAllRate,
 	}
 	for name, tt := range testTable {
 		tt := tt
