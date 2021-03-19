319d7bea44bb4b32d87c675d88c423e4b8a3f98d
Vazha Bezhanishvili <vazha.bezhanishvili@elegro.eu>
2021-03-19 13:36:23 +0200

    goimports

--- exchanges/lakebtc/lakebtc_websocket.go
+++ exchanges/lakebtc/lakebtc_websocket.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/thrasher-corp/gocryptotrader/common"
 	"github.com/thrasher-corp/gocryptotrader/currency"
-	"github.com/thrasher-corp/gocryptotrader/exchanges"
+	exchange "github.com/thrasher-corp/gocryptotrader/exchanges"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/asset"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/orderbook"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/stream"
