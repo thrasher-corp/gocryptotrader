a05bc4323e7434d1a38b01bd521ad06ac2db727b
Yordan Miladinov <yordan@numus.digital>
2021-07-30 13:26:02 +0300

    exchanges/bithumb: proper order.payment_currency key

--- exchanges/bithumb/bithumb.go
+++ exchanges/bithumb/bithumb.go
@@ -314,7 +314,7 @@ func (b *Bithumb) PlaceTrade(orderCurrency, transactionType string, units float6
 
 	params := url.Values{}
 	params.Set("order_currency", strings.ToUpper(orderCurrency))
-	params.Set("Payment_currency", "KRW")
+	params.Set("payment_currency", "KRW")
 	params.Set("type", strings.ToUpper(transactionType))
 	params.Set("units", strconv.FormatFloat(units, 'f', -1, 64))
 	params.Set("price", strconv.FormatInt(price, 10))
@@ -329,9 +329,7 @@ func (b *Bithumb) ModifyTrade(orderID, orderCurrency, transactionType string, un
 
 	params := url.Values{}
 	params.Set("order_currency", strings.ToUpper(orderCurrency))
-	// XXX: Why "payment_currency" starts with an uppercase letter?  On
-	// https://apidocs.bithumb.com/docs/place it's spelled with a lowercase 'p'.
-	params.Set("Payment_currency", "KRW")
+	params.Set("payment_currency", "KRW")
 	params.Set("type", strings.ToUpper(transactionType))
 	params.Set("units", strconv.FormatFloat(units, 'f', -1, 64))
 	params.Set("price", strconv.FormatInt(price, 10))
