fd8f49fded59fb4c6032169750c3ec586991cf1d
Jaydeep Rajpurohit <jaydeeppurohit1996@gmail.com>
2021-08-02 20:39:46 +0530

    adds API endpoint paths

--- exchanges/bybit/bybit.go
+++ exchanges/bybit/bybit.go
@@ -16,15 +16,24 @@ const (
 	// Public endpoints
 	// TODO:
 	bybitSpotGetSymbols = "/spot/v1/symbols"
-	bybitGetOrderBook = "/spot/quote/v1/depth"
-	bybitGetMergedOrderBook = "/spot/quote/v1/depth/merged"
-	bybitGetRecentTrades = "/spot/quote/v1/trades"
-	bybitGetCandlestickChart = "/spot/quote/v1/kline"
+	bybitOrderBook = "/spot/quote/v1/depth"
+	bybitMergedOrderBook = "/spot/quote/v1/depth/merged"
+	bybitRecentTrades = "/spot/quote/v1/trades"
+	bybitCandlestickChart = "/spot/quote/v1/kline"
+	bybit24HrsChange = "/spot/quote/v1/ticker/24hr"
+	bybitLastTradedPrice = "/spot/quote/v1/ticker/price"
+	bybitBestBidAskPrice = "/spot/quote/v1/ticker/book_ticker"
 
-	bybitGet24HrsChange = "/spot/quote/v1/ticker/24hr"
-	bybitGetLastTradedPrice = "/spot/quote/v1/ticker/price"
-	bybitGetBestBidAskPrice = "/spot/quote/v1/ticker/book_ticker"
 	// Authenticated endpoints
+	// TODO:
+	bybitAuthenticatedSpotOrder = "/spot/v1/order" // create, query, cancel
+	bybitBatchCancelSpotOrder = "/spot/order/batch-cancel"
+	bybitOpenOrder = "/spot/v1/open-orders"
+	bybitPastOrder = "/spot/v1/history-orders"
+	bybitTradeHistory = "/spot/v1/myTrades"
+
+	bybitWalletBalance = "/spot/v1/account"
+	bybitServerTime = "/spot/v1/time"
 )
 
 // Start implementing public and private exchange API funcs below
