0374c075afb35957c2913385d2c19d14eb32b8ab
shazbert <oharareid.ryan@gmail.com>
2021-09-24 08:47:03 +1000

    alert: fix commenting for its generalized purpose

--- exchanges/alert/alert.go
+++ exchanges/alert/alert.go
@@ -5,8 +5,8 @@ import (
 	"sync/atomic"
 )
 
-// Notice defines fields required to alert sub-systems of a change of state to
-// re-check depth list
+// Notice defines fields required to alert sub-systems of a change of state so a
+// routine can re-check in memory data
 type Notice struct {
 	// Channel to wait for an alert on.
 	forAlert chan struct{}
@@ -14,14 +14,14 @@ type Notice struct {
 	// alert.
 	sema uint32
 	// After closing the forAlert channel this will notify when all the routines
-	// that have waited, have either checked the orderbook depth or finished.
+	// that have waited, have completed their checks.
 	wg sync.WaitGroup
 	// Segregated lock only for waiting routines, so as this does not interfere
-	// with the main depth lock, acts as a rolling gate.
+	// with the main calling lock, this acts as a rolling gate.
 	m sync.Mutex
 }
 
-// Alert establishes a state change on the orderbook depth.
+// Alert establishes a state change on the required struct.
 func (n *Notice) Alert() {
 	// CompareAndSwap is used to swap from 1 -> 2 so we don't keep actuating
 	// the opposing compare and swap in method wait. This function can return
@@ -45,9 +45,9 @@ func (n *Notice) actuate() {
 	n.m.Unlock()
 }
 
-// Wait pauses calling routine until depth change has been established via depth
-// method alert. Kick allows for cancellation of waiting or when the caller
-// has been shut down, if this is not needed it can be set to nil. This
+// Wait pauses calling routine until change of state has been established via
+// notice method Alert. Kick allows for cancellation of waiting or when the
+// caller has been shut down, if this is not needed it can be set to nil. This
 // returns a channel so strategies can cleanly wait on a select statement case.
 func (n *Notice) Wait(kick <-chan struct{}) <-chan bool {
 	reply := make(chan bool)
@@ -61,8 +61,8 @@ func (n *Notice) Wait(kick <-chan struct{}) <-chan bool {
 	return reply
 }
 
-// hold waits on either channel in the event that the routine has finished or an
-// alert from a depth update has occurred.
+// hold waits on either channel in the event that the routine has
+// finished/canceled or an alert from a depth update has occurred.
 func (n *Notice) hold(ch chan<- bool, kick <-chan struct{}) {
 	select {
 	// In a select statement, if by chance there is no receiver or its late,
