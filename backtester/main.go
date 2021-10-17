a46701b56128ebcc74e1793ade6b04f25f2afbef
David Sabater Dinter <david.sabater@gmail.com>
2021-10-17 18:39:08 +0100

    Revert default strategy to dca

--- backtester/main.go
+++ backtester/main.go
@@ -30,7 +30,7 @@ func main() {
 			wd,
 			"config",
 			"examples",
-			"arbitrage-candles.strat"),
+			"dca-api-candles.strat"),
 		"the config containing strategy params")
 	flag.StringVar(
 		&templatePath,
