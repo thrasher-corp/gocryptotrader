6b67995d3475dab8ef6ef79e54cadb3d2c403f35
Shazbert <oharareid.ryan@gmail.com>
2022-02-03 12:03:29 +1100

    exchange: revert change

--- exchanges/exchange.go
+++ exchanges/exchange.go
@@ -422,7 +422,7 @@ func (b *Base) GetPairFormat(assetType asset.Item, requestFormat bool) (currency
 func (b *Base) GetEnabledPairs(a asset.Item) (currency.Pairs, error) {
 	err := b.CurrencyPairs.IsAssetEnabled(a)
 	if err != nil {
-		return nil, err
+		return nil, nil // nolint:nilerr // non-fatal error
 	}
 	format, err := b.GetPairFormat(a, false)
 	if err != nil {
