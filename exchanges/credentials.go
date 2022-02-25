963195a97b0afc016c09cba2121cbfb1b2b3cfdf
Ryan O'Hara-Reid <oharareid.ryan@gmail.com>
2022-02-25 11:11:45 +1100

    Update exchanges/credentials.go
    Co-authored-by: Scott <gloriousCode@users.noreply.github.com>

--- exchanges/credentials.go
+++ exchanges/credentials.go
@@ -132,7 +132,7 @@ func (c *Credentials) GetMetaData() (flag, values string) {
 }
 
 // IsEmpty return true if the underlying credentials type has not been filled
-// with atleast one item.
+// with at least one item.
 func (c *Credentials) IsEmpty() bool {
 	if c == nil {
 		return true
