600523c6a4b5d1bbfc4f4f3725d6f1561247dd2a
Shazbert <oharareid.ryan@gmail.com>
2022-03-23 15:21:49 +1100

    orderbook: Add unsafe methods and liquidity checks

--- exchanges/orderbook/unsafe_test.go
+++ exchanges/orderbook/unsafe_test.go
@@ -1,7 +1,9 @@
 package orderbook
 
 import (
+	"errors"
 	"testing"
+	"time"
 
 	"github.com/gofrs/uuid"
 )
@@ -14,6 +16,7 @@ func (e *externalBook) Lock()   {}
 func (e *externalBook) Unlock() {}
 
 func TestUnsafe(t *testing.T) {
+	t.Parallel()
 	d := NewDepth(unsafeID)
 	ob := d.GetUnsafe()
 	if ob.AskHead == nil || ob.BidHead == nil || ob.m == nil {
@@ -25,4 +28,159 @@ func TestUnsafe(t *testing.T) {
 	ob.Unlock() // nolint:staticcheck, gocritic // Not needed in test
 	ob.LockWith(ob2)
 	ob.UnlockWith(ob2)
+}
+
+func TestCheckBidLiquidity(t *testing.T) {
+	t.Parallel()
+	d := NewDepth(unsafeID)
+	unsafe := d.GetUnsafe()
+	err := unsafe.CheckBidLiquidity()
+	if !errors.Is(err, errNoLiquidity) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoLiquidity)
+	}
+
+	d.LoadSnapshot([]Item{{Price: 2}}, nil, 0, time.Time{}, false)
+	err = unsafe.CheckBidLiquidity()
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+}
+
+func TestCheckAskLiquidity(t *testing.T) {
+	t.Parallel()
+	d := NewDepth(unsafeID)
+	unsafe := d.GetUnsafe()
+	err := unsafe.CheckAskLiquidity()
+	if !errors.Is(err, errNoLiquidity) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoLiquidity)
+	}
+
+	d.LoadSnapshot(nil, []Item{{Price: 2}}, 0, time.Time{}, false)
+	err = unsafe.CheckAskLiquidity()
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+}
+
+func TestGetBestBid(t *testing.T) {
+	t.Parallel()
+	d := NewDepth(unsafeID)
+	unsafe := d.GetUnsafe()
+	_, err := unsafe.GetBestBid()
+	if !errors.Is(err, errNoLiquidity) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoLiquidity)
+	}
+
+	d.LoadSnapshot([]Item{{Price: 2}}, nil, 0, time.Time{}, false)
+	bestBid, err := unsafe.GetBestBid()
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+
+	if bestBid != 2 {
+		t.Fatal("unexpected value")
+	}
+}
+
+func TestGetBestAsk(t *testing.T) {
+	t.Parallel()
+	d := NewDepth(unsafeID)
+	unsafe := d.GetUnsafe()
+	_, err := unsafe.GetBestAsk()
+	if !errors.Is(err, errNoLiquidity) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoLiquidity)
+	}
+
+	d.LoadSnapshot(nil, []Item{{Price: 2}}, 0, time.Time{}, false)
+	bestAsk, err := unsafe.GetBestAsk()
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+
+	if bestAsk != 2 {
+		t.Fatal("unexpected value")
+	}
+}
+
+func TestGetMidPrice(t *testing.T) {
+	t.Parallel()
+	d := NewDepth(unsafeID)
+	unsafe := d.GetUnsafe()
+	_, err := unsafe.GetMidPrice()
+	if !errors.Is(err, errNoLiquidity) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoLiquidity)
+	}
+
+	d.LoadSnapshot([]Item{{Price: 1}}, []Item{{Price: 2}}, 0, time.Time{}, false)
+	mid, err := unsafe.GetMidPrice()
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+
+	if mid != 1.5 {
+		t.Fatal("unexpected value")
+	}
+}
+
+func TestGetSpread(t *testing.T) {
+	t.Parallel()
+	d := NewDepth(unsafeID)
+	unsafe := d.GetUnsafe()
+	_, err := unsafe.GetSpread()
+	if !errors.Is(err, errNoLiquidity) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoLiquidity)
+	}
+
+	d.LoadSnapshot([]Item{{Price: 1}}, []Item{{Price: 2}}, 0, time.Time{}, false)
+	spread, err := unsafe.GetSpread()
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+
+	if spread != 1 {
+		t.Fatal("unexpected value")
+	}
+}
+
+func TestGetImbalance(t *testing.T) {
+	t.Parallel()
+	d := NewDepth(unsafeID)
+	unsafe := d.GetUnsafe()
+	_, err := unsafe.GetImbalance()
+	if !errors.Is(err, errNoLiquidity) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoLiquidity)
+	}
+
+	// balance skewed to asks
+	d.LoadSnapshot([]Item{{Price: 1, Amount: 1}}, []Item{{Price: 2, Amount: 1000}}, 0, time.Time{}, false)
+	imbalance, err := unsafe.GetImbalance()
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+
+	if imbalance != -0.998001998001998 {
+		t.Fatal("unexpected value")
+	}
+
+	// balance skewed to bids
+	d.LoadSnapshot([]Item{{Price: 1, Amount: 1000}}, []Item{{Price: 2, Amount: 1}}, 0, time.Time{}, false)
+	imbalance, err = unsafe.GetImbalance()
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+
+	if imbalance != 0.998001998001998 {
+		t.Fatal("unexpected value")
+	}
+
+	// in balance
+	d.LoadSnapshot([]Item{{Price: 1, Amount: 1}}, []Item{{Price: 2, Amount: 1}}, 0, time.Time{}, false)
+	imbalance, err = unsafe.GetImbalance()
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+
+	if imbalance != 0 {
+		t.Fatal("unexpected value")
+	}
 }
