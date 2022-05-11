8072ef1c95d2ba5788f8a29e8bc9f1b85bc3171d
samuael <samuaeladnew@gmail.com>
2022-05-11 09:49:04 +0300

    CI: Fix reverting unnessesary changes and type conversion issues

--- exchanges/support_test.go
+++ exchanges/support_test.go
@@ -3,7 +3,7 @@ package exchange
 import "testing"
 
 func TestIsSupported(t *testing.T) {
-	if ok := IsSupported("Binanceus"); !ok {
+	if ok := IsSupported("BiTStaMp"); !ok {
 		t.Error("supported exchange should be valid")
 	}
 
