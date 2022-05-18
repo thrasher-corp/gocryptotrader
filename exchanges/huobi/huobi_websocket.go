995af158dbdd1df027072e59003b4c88595df5ef
Ryan O'Hara-Reid <ryan.oharareid@thrasher.io>
2022-05-18 12:46:25 +1000

    merge: fixes

--- exchanges/huobi/huobi_websocket.go
+++ exchanges/huobi/huobi_websocket.go
@@ -15,7 +15,6 @@ import (
 	"github.com/thrasher-corp/gocryptotrader/common"
 	"github.com/thrasher-corp/gocryptotrader/common/crypto"
 	"github.com/thrasher-corp/gocryptotrader/currency"
-	exchange "github.com/thrasher-corp/gocryptotrader/exchanges"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/account"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/asset"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/order"
