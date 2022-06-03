e5d073c46377a6f89cad8df424f595c08cd2d5f2
Ryan O'Hara-Reid <ryan.oharareid@thrasher.io>
2022-06-04 07:38:32 +1000

    kline: Add tests

--- exchanges/kline/technical_analysis.go
+++ exchanges/kline/technical_analysis.go
@@ -1,6 +1,19 @@
 package kline
 
-import "github.com/thrasher-corp/gct-ta/indicators"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/thrasher-corp/gct-ta/indicators"
+)
+
+var (
+	errInvalidPeriod              = errors.New("invalid period")
+	errNoData                     = errors.New("no data")
+	errInvalidDeviationMultiplier = errors.New("invalid deviation multiplier")
+	errNilOHLC                    = errors.New("nil OHLC data")
+	errInvalidDataSetLengths      = errors.New("invalid data set lengths")
+)
 
 // OHLC is a connector for technical analysis usage
 type OHLC struct {
@@ -11,8 +24,8 @@ type OHLC struct {
 	Volume []float64
 }
 
-// GetOHLC returns the entire subset of candles as a friendly type for technical
-// gct technical analysis usage.
+// GetOHLC returns the entire subset of candles as a friendly type for gct
+// technical analysis usage.
 func (i *Item) GetOHLC() *OHLC {
 	open := make([]float64, len(i.Candles))
 	high := make([]float64, len(i.Candles))
@@ -31,53 +44,186 @@ func (i *Item) GetOHLC() *OHLC {
 
 // GetAverageTrueRange returns the Average True Range for the given period.
 func (o *OHLC) GetAverageTrueRange(period int) ([]float64, error) {
+	if o == nil {
+		return nil, fmt.Errorf("get average true range %w", errNilOHLC)
+	}
+	if period <= 0 {
+		return nil, fmt.Errorf("get average true range %w", errInvalidPeriod)
+	}
+	if len(o.High) == 0 {
+		return nil, fmt.Errorf("get average true range high %w", errNoData)
+	}
+	if len(o.Low) == 0 {
+		return nil, fmt.Errorf("get average true range low %w", errNoData)
+	}
+	if len(o.Close) == 0 {
+		return nil, fmt.Errorf("get average true range close %w", errNoData)
+	}
 	return indicators.ATR(o.High, o.Low, o.Close, period), nil
 }
 
 // GetBollingerBands returns Bollinger Bands for the given period.
-func (o *OHLC) GetBollingerBands(options []float64, period int, nbDevUp, nbDevDown float64, m indicators.MaType) (upper, middle, lower []float64, err error) {
-	upper, middle, lower = indicators.BBANDS(options, period, nbDevUp, nbDevDown, m)
+func (o *OHLC) GetBollingerBands(period int, nbDevUp, nbDevDown float64, m indicators.MaType) (upper, middle, lower []float64, err error) {
+	if o == nil {
+		return nil, nil, nil, fmt.Errorf("get bollinger bands %w", errNilOHLC)
+	}
+	if period <= 0 {
+		return nil, nil, nil, fmt.Errorf("get bollinger bands %w", errInvalidPeriod)
+	}
+	if nbDevUp <= 0 {
+		return nil, nil, nil, fmt.Errorf("get bollinger bands %w upper limit", errInvalidDeviationMultiplier)
+	}
+	if nbDevDown <= 0 {
+		return nil, nil, nil, fmt.Errorf("get bollinger bands %w lower limit", errInvalidDeviationMultiplier)
+	}
+	if len(o.Close) == 0 {
+		return nil, nil, nil, fmt.Errorf("get bollinger bands close %w", errNoData)
+	}
+	if period > len(o.Close) { // TODO: Investigate the panic when this protection is removed.
+		return nil, nil, nil, fmt.Errorf("get bollinger bands %w '%v' should not exceed close data length '%v'",
+			errInvalidPeriod, period, len(o.Close))
+	}
+	upper, middle, lower = indicators.BBANDS(o.Close, period, nbDevUp, nbDevDown, m)
 	return
 }
 
 // GetCorrelationCoefficient returns GetCorrelation Coefficient against another
 // candle data set for the given period.
 func (o *OHLC) GetCorrelationCoefficient(other *OHLC, period int) ([]float64, error) {
+	if o == nil {
+		return nil, fmt.Errorf("get correlation coefficient %w", errNilOHLC)
+	}
+	if period <= 0 {
+		return nil, fmt.Errorf("get correlation coefficient %w", errInvalidPeriod)
+	}
+	if other == nil {
+		return nil, fmt.Errorf("get correlation coefficient %w", errNilOHLC)
+	}
+	if len(o.Close) == 0 {
+		return nil, fmt.Errorf("get correlation coefficient close %w", errNoData)
+	}
+	if len(other.Close) == 0 {
+		return nil, fmt.Errorf("get correlation coefficient comparison close %w", errNoData)
+	}
 	return indicators.CorrelationCoefficient(o.Close, other.Close, period), nil
 }
 
 // GetSimpleMovingAverage returns MA for the supplied price set for the given
 // period.
 func (o *OHLC) GetSimpleMovingAverage(option []float64, period int) ([]float64, error) {
-	return indicators.SMA(o.Close, period), nil
+	if o == nil {
+		return nil, fmt.Errorf("get simple moving average %w", errNilOHLC)
+	}
+	if period <= 0 {
+		return nil, fmt.Errorf("get simple moving average %w", errInvalidPeriod)
+	}
+	if len(option) == 0 {
+		return nil, fmt.Errorf("get simple moving average %w", errNoData)
+	}
+	return indicators.SMA(option, period), nil
 }
 
 // GetExponentialMovingAverage returns the EMA on the supplied price set for the
 // given period.
 func (o *OHLC) GetExponentialMovingAverage(option []float64, period int) ([]float64, error) {
-	return indicators.EMA(o.Close, period), nil
+	if o == nil {
+		return nil, fmt.Errorf("get exponential moving average %w", errNilOHLC)
+	}
+	if period <= 0 {
+		return nil, fmt.Errorf("get exponential moving average %w", errInvalidPeriod)
+	}
+	if len(option) == 0 {
+		return nil, fmt.Errorf("get exponential moving average %w", errNoData)
+	}
+	return indicators.EMA(option, period), nil
 }
 
 // GetMovingAverageConvergenceDivergence returns the
 // MACD (macd, signal period vals, histogram) for the given price
 // set and the paramaters fast, slow signal time periods.
 func (o *OHLC) GetMovingAverageConvergenceDivergence(option []float64, fast, slow, signal int) (macd, signalVals, histogram []float64, err error) {
+	if o == nil {
+		return nil, nil, nil, fmt.Errorf("get macd %w", errNilOHLC)
+	}
+	if fast <= 0 {
+		return nil, nil, nil, fmt.Errorf("get macd %w fast", errInvalidPeriod)
+	}
+	if slow <= 0 {
+		return nil, nil, nil, fmt.Errorf("get macd %w slow", errInvalidPeriod)
+	}
+	if signal <= 0 {
+		return nil, nil, nil, fmt.Errorf("get macd %w signal", errInvalidPeriod)
+	}
+	if len(option) == 0 {
+		return nil, nil, nil, fmt.Errorf("get macd %w", errNoData)
+	}
 	macd, signalVals, histogram = indicators.MACD(option, fast, slow, signal)
 	return
 }
 
 // GetMoneyFlowIndex returns Money Flow Index for the given period.
 func (o *OHLC) GetMoneyFlowIndex(period int) ([]float64, error) {
+	if o == nil {
+		return nil, fmt.Errorf("get money flow index %w", errNilOHLC)
+	}
+	if period <= 0 {
+		return nil, fmt.Errorf("get money flow index %w", errInvalidPeriod)
+	}
+	highLen := len(o.High)
+	if highLen == 0 {
+		return nil, fmt.Errorf("get money flow index %w for high", errNoData)
+	}
+	lowLen := len(o.Low)
+	if lowLen == 0 {
+		return nil, fmt.Errorf("get money flow index %w for low", errNoData)
+	}
+	closeLen := len(o.Close)
+	if closeLen == 0 {
+		return nil, fmt.Errorf("get money flow index %w for close", errNoData)
+	}
+	volLen := len(o.Volume)
+	if volLen == 0 {
+		return nil, fmt.Errorf("get money flow index %w for volume", errNoData)
+	}
+	if highLen != closeLen || lowLen != closeLen || volLen != closeLen {
+		// TODO: Investigate the panic when this protection is removed.
+		// This is very unstable with incorrect lengths.
+		return nil, fmt.Errorf("get money flow index %w", errInvalidDataSetLengths)
+	}
+
+	if period >= len(o.Close) {
+		// TODO: Investigate the panic when this protection is removed.
+		return nil, fmt.Errorf("get money flow index %w '%v' should not exceed or equal close data length '%v'",
+			errInvalidPeriod, period, len(o.Close))
+	}
 	return indicators.MFI(o.High, o.Low, o.Close, o.Volume, period), nil
 }
 
 // GetOnBalanceVolume returns On Balance Volume.
 func (o *OHLC) GetOnBalanceVolume() ([]float64, error) {
+	if o == nil {
+		return nil, fmt.Errorf("get on balance volume %w", errNilOHLC)
+	}
+	if len(o.Close) == 0 {
+		return nil, fmt.Errorf("get on balance volume %w for close", errNoData)
+	}
+	if len(o.Volume) == 0 {
+		return nil, fmt.Errorf("get on balance volume %w for volume", errNoData)
+	}
 	return indicators.OBV(o.Close, o.Volume), nil
 }
 
 // GetRelativeStrengthIndex returns the relative strength index from the the
 // given price set and period.
 func (o *OHLC) GetRelativeStrengthIndex(option []float64, period int) ([]float64, error) {
+	if o == nil {
+		return nil, fmt.Errorf("get relative strength index %w", errNilOHLC)
+	}
+	if period <= 0 {
+		return nil, fmt.Errorf("get relative strength index %w", errInvalidPeriod)
+	}
+	if len(option) == 0 {
+		return nil, fmt.Errorf("get relative strength index %w", errNoData)
+	}
 	return indicators.RSI(option, period), nil
 }
---
+++ exchanges/kline/technical_analysis_test.go
@@ -0,0 +1,329 @@
+package kline
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestGetOHLC(t *testing.T) {
+	t.Parallel()
+	if (&Item{Candles: []Candle{{Open: 1337}}}).GetOHLC() == nil {
+		t.Fatal("unexpected value")
+	}
+}
+
+func TestGetAverageTrueRange(t *testing.T) {
+	t.Parallel()
+
+	var ohlc *OHLC
+	_, err := ohlc.GetAverageTrueRange(0)
+	if !errors.Is(err, errNilOHLC) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNilOHLC)
+	}
+
+	ohlc = &OHLC{}
+	_, err = ohlc.GetAverageTrueRange(0)
+	if !errors.Is(err, errInvalidPeriod) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errInvalidPeriod)
+	}
+
+	_, err = ohlc.GetAverageTrueRange(9)
+	if !errors.Is(err, errNoData) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoData)
+	}
+
+	ohlc.High = append(ohlc.High, 1337)
+	_, err = ohlc.GetAverageTrueRange(9)
+	if !errors.Is(err, errNoData) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoData)
+	}
+
+	ohlc.Low = append(ohlc.Low, 1337)
+	_, err = ohlc.GetAverageTrueRange(9)
+	if !errors.Is(err, errNoData) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoData)
+	}
+
+	ohlc.Close = append(ohlc.Close, 1337)
+	_, err = ohlc.GetAverageTrueRange(9)
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+}
+
+func TestGetBollingerBands(t *testing.T) {
+	t.Parallel()
+
+	var ohlc *OHLC
+	_, _, _, err := ohlc.GetBollingerBands(0, 0, 0, 5)
+	if !errors.Is(err, errNilOHLC) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNilOHLC)
+	}
+
+	ohlc = &OHLC{}
+	_, _, _, err = ohlc.GetBollingerBands(0, 0, 0, 5)
+	if !errors.Is(err, errInvalidPeriod) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errInvalidPeriod)
+	}
+
+	_, _, _, err = ohlc.GetBollingerBands(9, 0, 0, 5)
+	if !errors.Is(err, errInvalidDeviationMultiplier) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errInvalidDeviationMultiplier)
+	}
+
+	_, _, _, err = ohlc.GetBollingerBands(9, 1, 0, 5)
+	if !errors.Is(err, errInvalidDeviationMultiplier) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errInvalidDeviationMultiplier)
+	}
+
+	_, _, _, err = ohlc.GetBollingerBands(9, 1, 1, 5)
+	if !errors.Is(err, errNoData) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoData)
+	}
+
+	ohlc.Close = append(ohlc.Close, 1337, 1337, 1337, 1337, 1337, 1337, 1337, 1337, 1337)
+	_, _, _, err = ohlc.GetBollingerBands(10, 1, 1, 5)
+	if !errors.Is(err, errInvalidPeriod) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errInvalidPeriod)
+	}
+
+	_, _, _, err = ohlc.GetBollingerBands(9, 1, 1, 5)
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+}
+
+func TestGetCorrelationCoefficient(t *testing.T) {
+	t.Parallel()
+
+	var ohlc *OHLC
+	_, err := ohlc.GetCorrelationCoefficient(nil, 0)
+	if !errors.Is(err, errNilOHLC) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNilOHLC)
+	}
+
+	ohlc = &OHLC{}
+	_, err = ohlc.GetCorrelationCoefficient(nil, 0)
+	if !errors.Is(err, errInvalidPeriod) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errInvalidPeriod)
+	}
+
+	_, err = ohlc.GetCorrelationCoefficient(nil, 9)
+	if !errors.Is(err, errNilOHLC) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNilOHLC)
+	}
+
+	_, err = ohlc.GetCorrelationCoefficient(&OHLC{}, 9)
+	if !errors.Is(err, errNoData) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoData)
+	}
+
+	ohlc.Close = append(ohlc.Close, 1337)
+	_, err = ohlc.GetCorrelationCoefficient(&OHLC{}, 9)
+	if !errors.Is(err, errNoData) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoData)
+	}
+
+	_, err = ohlc.GetCorrelationCoefficient(&OHLC{Close: []float64{1337}}, 9)
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+}
+
+func TestGetSimpleMovingAverage(t *testing.T) {
+	t.Parallel()
+
+	var ohlc *OHLC
+	_, err := ohlc.GetSimpleMovingAverage(nil, 0)
+	if !errors.Is(err, errNilOHLC) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNilOHLC)
+	}
+
+	ohlc = &OHLC{}
+	_, err = ohlc.GetSimpleMovingAverage(nil, 0)
+	if !errors.Is(err, errInvalidPeriod) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errInvalidPeriod)
+	}
+
+	_, err = ohlc.GetSimpleMovingAverage(nil, 9)
+	if !errors.Is(err, errNoData) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoData)
+	}
+
+	_, err = ohlc.GetSimpleMovingAverage([]float64{1337}, 9)
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+}
+
+func TestGetExponentialMovingAverage(t *testing.T) {
+	t.Parallel()
+
+	var ohlc *OHLC
+	_, err := ohlc.GetExponentialMovingAverage(nil, 0)
+	if !errors.Is(err, errNilOHLC) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNilOHLC)
+	}
+
+	ohlc = &OHLC{}
+	_, err = ohlc.GetExponentialMovingAverage(nil, 0)
+	if !errors.Is(err, errInvalidPeriod) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errInvalidPeriod)
+	}
+
+	_, err = ohlc.GetExponentialMovingAverage(nil, 9)
+	if !errors.Is(err, errNoData) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoData)
+	}
+
+	_, err = ohlc.GetExponentialMovingAverage([]float64{1337}, 9)
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+}
+
+func TestGetMovingAverageConvergenceDivergence(t *testing.T) {
+	t.Parallel()
+
+	var ohlc *OHLC
+	_, _, _, err := ohlc.GetMovingAverageConvergenceDivergence(nil, 0, 0, 0)
+	if !errors.Is(err, errNilOHLC) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNilOHLC)
+	}
+
+	ohlc = &OHLC{}
+	_, _, _, err = ohlc.GetMovingAverageConvergenceDivergence(nil, 0, 0, 0)
+	if !errors.Is(err, errInvalidPeriod) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errInvalidPeriod)
+	}
+
+	_, _, _, err = ohlc.GetMovingAverageConvergenceDivergence(nil, 1, 0, 0)
+	if !errors.Is(err, errInvalidPeriod) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errInvalidPeriod)
+	}
+
+	_, _, _, err = ohlc.GetMovingAverageConvergenceDivergence(nil, 1, 1, 0)
+	if !errors.Is(err, errInvalidPeriod) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errInvalidPeriod)
+	}
+
+	_, _, _, err = ohlc.GetMovingAverageConvergenceDivergence(nil, 1, 1, 1)
+	if !errors.Is(err, errNoData) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoData)
+	}
+
+	_, _, _, err = ohlc.GetMovingAverageConvergenceDivergence([]float64{1337}, 1, 1, 9)
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+}
+
+func TestGetMoneyFlowIndex(t *testing.T) {
+	t.Parallel()
+
+	var ohlc *OHLC
+	_, err := ohlc.GetMoneyFlowIndex(0)
+	if !errors.Is(err, errNilOHLC) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNilOHLC)
+	}
+
+	ohlc = &OHLC{}
+	_, err = ohlc.GetMoneyFlowIndex(0)
+	if !errors.Is(err, errInvalidPeriod) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errInvalidPeriod)
+	}
+
+	_, err = ohlc.GetMoneyFlowIndex(9)
+	if !errors.Is(err, errNoData) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoData)
+	}
+
+	ohlc.High = append(ohlc.High, 1337, 1337, 1337, 1337, 1337, 1337)
+	_, err = ohlc.GetMoneyFlowIndex(9)
+	if !errors.Is(err, errNoData) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoData)
+	}
+
+	ohlc.Low = append(ohlc.Low, 1337, 1337, 1337, 1337, 1337, 1337)
+	_, err = ohlc.GetMoneyFlowIndex(9)
+	if !errors.Is(err, errNoData) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoData)
+	}
+
+	ohlc.Close = append(ohlc.Close, 1337, 1337, 1337, 1337, 1337, 1337)
+	_, err = ohlc.GetMoneyFlowIndex(9)
+	if !errors.Is(err, errNoData) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoData)
+	}
+
+	ohlc.Volume = append(ohlc.Volume, 1337, 1337, 1337, 1337, 1337)
+	_, err = ohlc.GetMoneyFlowIndex(5)
+	if !errors.Is(err, errInvalidDataSetLengths) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errInvalidDataSetLengths)
+	}
+
+	ohlc.Volume = append(ohlc.Volume, 1337)
+	_, err = ohlc.GetMoneyFlowIndex(6)
+	if !errors.Is(err, errInvalidPeriod) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errInvalidPeriod)
+	}
+
+	_, err = ohlc.GetMoneyFlowIndex(3)
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+}
+
+func TestGetOnBalanceVolume(t *testing.T) {
+	t.Parallel()
+
+	var ohlc *OHLC
+	_, err := ohlc.GetOnBalanceVolume()
+	if !errors.Is(err, errNilOHLC) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNilOHLC)
+	}
+
+	ohlc = &OHLC{}
+	_, err = ohlc.GetOnBalanceVolume()
+	if !errors.Is(err, errNoData) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoData)
+	}
+
+	ohlc.Close = append(ohlc.Close, 1337, 1337, 1337, 1337, 1337, 1337)
+	_, err = ohlc.GetOnBalanceVolume()
+	if !errors.Is(err, errNoData) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoData)
+	}
+
+	ohlc.Volume = append(ohlc.Volume, 0.00000001)
+	_, err = ohlc.GetOnBalanceVolume()
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+}
+
+func TestGetRelativeStrengthIndex(t *testing.T) {
+	t.Parallel()
+
+	var ohlc *OHLC
+	_, err := ohlc.GetRelativeStrengthIndex(nil, 0)
+	if !errors.Is(err, errNilOHLC) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNilOHLC)
+	}
+
+	ohlc = &OHLC{}
+	_, err = ohlc.GetRelativeStrengthIndex(nil, 0)
+	if !errors.Is(err, errInvalidPeriod) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errInvalidPeriod)
+	}
+
+	_, err = ohlc.GetRelativeStrengthIndex(nil, 9)
+	if !errors.Is(err, errNoData) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, errNoData)
+	}
+
+	_, err = ohlc.GetRelativeStrengthIndex([]float64{1337}, 9)
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+}
