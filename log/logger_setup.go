b44a6cd4f72945dd76f0400c0a4a0aec5b3f3f23
Ryan O'Hara-Reid <ryan.oharareid@thrasher.io>
2022-08-09 11:32:11 +1000

    logger: segregate config.json from internal log.Config

--- log/logger_setup.go
+++ log/logger_setup.go
@@ -73,13 +73,18 @@ func GenDefaultSettings() *Config {
 }
 
 // SetGlobalLogConfig sets the global config with the supplied config
-func SetGlobalLogConfig(config *Config) error {
-	if config == nil {
+func SetGlobalLogConfig(incoming *Config) error {
+	if incoming == nil {
 		return errConfigNil
 	}
+	fileConf := *incoming.LoggerFileConfig
+	subs := make([]SubLoggerConfig, len(incoming.SubLoggers))
+	copy(subs, incoming.SubLoggers)
 	mu.Lock()
 	defer mu.Unlock()
-	globalLogConfig = config
+	globalLogConfig.SubLoggerConfig = incoming.SubLoggerConfig
+	globalLogConfig.Enabled = convert.BoolPtr(incoming.Enabled != nil && *incoming.Enabled)
+	globalLogConfig.LoggerFileConfig = &fileConf
 	return nil
 }
 
