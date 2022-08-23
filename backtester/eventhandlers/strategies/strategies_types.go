10f7ff323664a4ff2c74987fb91c5cfe644e553c
Scott <gloriousCode@users.noreply.github.com>
2022-08-23 14:22:06 +1000

    backtester: custom strategy plugins (#989)
    * Adds custom strategy
    
    * docs and structure
    
    * docs
    
    * rn
    
    * Documents plugins, adds custom strat config
    
    * mini fixes. Fleshes strategy test
    
    * docgen
    
    * Updates plugins to allow for multiple strategies to be loaded
    
    * docs
    
    * docs regen
    
    * fix doc accuracy
    
    * why did I add the word custom?

--- backtester/eventhandlers/strategies/strategies_types.go
+++ backtester/eventhandlers/strategies/strategies_types.go
@@ -1,11 +1,19 @@
 package strategies
 
 import (
+	"errors"
+
 	"github.com/thrasher-corp/gocryptotrader/backtester/data"
 	"github.com/thrasher-corp/gocryptotrader/backtester/eventhandlers/portfolio"
 	"github.com/thrasher-corp/gocryptotrader/backtester/eventtypes/signal"
 	"github.com/thrasher-corp/gocryptotrader/backtester/funding"
 )
+
+// ErrStrategyAlreadyExists returned when a strategy matches the same name
+var ErrStrategyAlreadyExists = errors.New("strategy already exists")
+
+// StrategyHolder holds strategies
+type StrategyHolder []Handler
 
 // Handler defines all functions required to run strategies against data events
 type Handler interface {
