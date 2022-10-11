b1522c0ed73a983e898b84ffaf47f1589253d4a1
Ryan O'Hara-Reid <ryan.oharareid@thrasher.io>
2022-10-12 09:11:09 +1100

    linter: fix

--- exchanges/exchange_test.go
+++ exchanges/exchange_test.go
@@ -16,7 +16,6 @@ import (
 	"github.com/thrasher-corp/gocryptotrader/exchanges/protocol"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/request"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/stream"
-	"github.com/thrasher-corp/gocryptotrader/log"
 	"github.com/thrasher-corp/gocryptotrader/portfolio/banking"
 )
 
