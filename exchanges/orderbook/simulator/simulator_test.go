9acbdbf203aa61f1ff8630698792cae353290e8c
Ryan O'Hara-Reid <oharareid.ryan@gmail.com>
2022-10-14 16:43:37 +1100

    depth: Add methods to derive liquidity allowances on orderbooks by volume and slippage (#962)
    * depth: methods to derive liquidity impact details
    
    * depth: fix comments on link list methods
    
    * depth: fix whoopsie
    
    * Update exchanges/orderbook/linked_list_test.go
    
    Co-authored-by: Scott <gloriousCode@users.noreply.github.com>
    
    * glorious: nits
    
    * orderbook: standardise methods to GCT math package
    
    * averagePrice: implementation (WIP)
    
    * ll: hmmmmmm
    
    * continued
    
    * orderbook: reworked functions
    
    * WIP
    
    * orderbook: add tests link up with RPC
    
    * orderbook: refined calculations, add tests (WIP)
    
    * orderbook: add tests finalise/verify remove state until next PR if needed
    
    * rpcserver/orderbook: remove redundant type and change wording
    
    * linter: fix
    
    * Update exchanges/orderbook/linked_list.go
    
    Co-authored-by: Scott <gloriousCode@users.noreply.github.com>
    
    * gctcli: noobed it up
    
    * orderbook: work work work (yesterday)
    
    * depth: WIP and testing
    
    * orderbook: improve calculations for bids traversal
    
    * orderbook: adjust tests
    
    * orderbook: linters/nits
    
    * orderbook: fix error returns and add asset to whalebomb
    
    * orderbook: drop error when full book is potentially consumed
    
    * Update cmd/gctcli/orderbook.go
    
    Co-authored-by: Scott <gloriousCode@users.noreply.github.com>
    
    * orderbook: Add tests and nits
    
    * glorious: nits
    
    * backtester: handle new errors
    
    * grpc: linter
    
    * orderbook: remove pesky t.Log()s
    
    * glorious: nits (yesterday)
    
    * depth/gctcli: Add in purchase requirements into orderbook movement, will also standardize in next commit after tests are fixed.
    
    * orderbook: standardize and overhaul
    
    * orderbook: update comments, update tests
    
    * rpcserver: add average ordercost and amounts
    
    * depth: add spread and imbalance methods
    
    * linter: fix
    
    * glorious: nits
    
    * orderbook: don't purge price, rn test.
    
    * glorious: codes nits
    
    * linter: fix
    
    * Update exchanges/orderbook/linked_list.go
    
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>
    
    * Update exchanges/orderbook/linked_list.go
    
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>
    
    * Update exchanges/orderbook/linked_list.go
    
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>
    
    * Update exchanges/orderbook/linked_list.go
    
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>
    
    * Update exchanges/orderbook/linked_list.go
    
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>
    
    * thrasher: nits
    
    Co-authored-by: Scott <gloriousCode@users.noreply.github.com>
    Co-authored-by: Ryan O'Hara-Reid <ryan.oharareid@thrasher.io>
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>

--- exchanges/orderbook/simulator/simulator_test.go
+++ exchanges/orderbook/simulator/simulator_test.go
@@ -12,14 +12,18 @@ import (
 func TestSimulate(t *testing.T) {
 	b := bitstamp.Bitstamp{}
 	b.SetDefaults()
+	b.Verbose = false
 	o, err := b.FetchOrderbook(context.Background(),
 		currency.NewPair(currency.BTC, currency.USD), asset.Spot)
 	if err != nil {
 		t.Error(err)
 	}
-
-	r := o.SimulateOrder(10000000, true)
-	t.Log(r.Status)
-	r = o.SimulateOrder(2171, false)
-	t.Log(r.Status)
+	_, err = o.SimulateOrder(10000000, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = o.SimulateOrder(2171, false)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
