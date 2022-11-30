8f58504eaafeb083120c69861f40dd64035a8dee
Ryan O'Hara-Reid <ryan.oharareid@thrasher.io>
2022-11-30 16:06:21 +1100

    engine: fix tests

--- engine/datahistory_manager_test.go
+++ engine/datahistory_manager_test.go
@@ -1163,7 +1163,7 @@ func TestUpscaleJobCandleData(t *testing.T) {
 		Exchange:           testExchange,
 		Asset:              asset.Spot,
 		Pair:               currency.NewPair(currency.BTC, currency.USDT),
-		StartDate:          time.Now().Add(-kline.OneHour.Duration() * 2),
+		StartDate:          time.Now().Add(-kline.OneHour.Duration() * 24),
 		EndDate:            time.Now(),
 		Interval:           kline.OneHour,
 		ConversionInterval: kline.OneDay,
@@ -1531,21 +1531,25 @@ func dataHistoryTraderLoader(exch, a, base, quote string, start, _ time.Time) ([
 }
 
 func dataHistoryCandleLoader(exch string, cp currency.Pair, a asset.Item, i kline.Interval, start, _ time.Time) (*kline.Item, error) {
+	start = start.Truncate(i.Duration())
+	var candles []kline.Candle
+	for x := 0; x < 24; x++ {
+		candles = append(candles, kline.Candle{
+			Time:   start,
+			Open:   1,
+			High:   10,
+			Low:    1,
+			Close:  4,
+			Volume: 8,
+		})
+		start = start.Add(i.Duration())
+	}
 	return &kline.Item{
 		Exchange: exch,
 		Pair:     cp,
 		Asset:    a,
 		Interval: i,
-		Candles: []kline.Candle{
-			{
-				Time:   start,
-				Open:   1,
-				High:   10,
-				Low:    1,
-				Close:  4,
-				Volume: 8,
-			},
-		},
+		Candles:  candles,
 	}, nil
 }
 
