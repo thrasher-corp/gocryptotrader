7ea8d0fc7275d68141abc3bbd689520c515a6cc6
Shazbert <oharareid.ryan@gmail.com>
2022-12-02 08:45:17 +1100

    reverse fix because this is a whoopsie

--- exchanges/kline/kline.go
+++ exchanges/kline/kline.go
@@ -406,7 +406,7 @@ func CalculateCandleDateRanges(start, end time.Time, interval Interval, limit ui
 // date inside the existing limits
 func (h *IntervalRangeHolder) HasDataAtDate(t time.Time) bool {
 	tu := t.Unix()
-	if tu < h.Start.Ticks || tu >= h.End.Ticks {
+	if tu < h.Start.Ticks || tu > h.End.Ticks {
 		return false
 	}
 	for i := range h.Ranges {
