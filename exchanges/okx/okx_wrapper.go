e50f5706903b9fa69feab0bc0c9be0b73b4d42b0
Ryan O'Hara-Reid <ryan.oharareid@thrasher.io>
2023-05-01 14:52:15 +1000

    okx: temp filter for available transfer chain

--- exchanges/okx/okx_wrapper.go
+++ exchanges/okx/okx_wrapper.go
@@ -1454,7 +1454,12 @@ func (ok *Okx) GetAvailableTransferChains(ctx context.Context, cryptocurrency cu
 	chains := make([]string, 0, len(currencyChains))
 	for x := range currencyChains {
 		if (!cryptocurrency.IsEmpty() && !strings.EqualFold(cryptocurrency.String(), currencyChains[x].Currency)) ||
-			(!currencyChains[x].CanDeposit && !currencyChains[x].CanWithdraw) {
+			(!currencyChains[x].CanDeposit && !currencyChains[x].CanWithdraw) ||
+			// Lightning network is currently not supported by transfer chains
+			// as it is an invoice string which is generated per request and is
+			// not a static address. TODO: Add a hook to generate a new invoice
+			// string per request.
+			(currencyChains[x].Chain != "" && currencyChains[x].Chain == "BTC-Lightning") {
 			continue
 		}
 		chains = append(chains, currencyChains[x].Chain)
