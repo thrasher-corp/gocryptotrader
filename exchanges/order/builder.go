ce9ce1e1b8ec5b122170e17ad665e5305aec1060
Ryan O'Hara-Reid <ryan.oharareid@thrasher.io>
2023-06-09 10:40:45 +1000

    Changes

--- exchanges/order/builder.go
+++ exchanges/order/builder.go
@@ -1,44 +1,35 @@
 package order
 
 // SubmissionConfig defines the order submission configuration for an exchange.
-// This allows for a more generic approach to submitting orders.
+// This struct allows for a more generic approach to submitting orders.
 type SubmissionConfig struct {
-	// OrderBaseAmountsRequired refers to an exchange that uses the base currency
-	// only as the amount for an order.
+	// OrderBaseAmountsRequired indicates whether the exchange requires the base
+	// currencyonly as the amount for an order.
 	OrderBaseAmountsRequired bool
-	// OrderSellingAmountsRequired refers to an exchange that changes the the
-	// amount identity when selling or buying. E.g. Bybit BTC-USD when
-	// bidding/buying BTC the amount is the selling currency USDT, but when
-	// asking/selling, the amount is BTC.
+	// OrderSellingAmountsRequired indicates whether the exchange changes the
+	// amount identity when selling or buying. For example, some exchanges use a
+	// different currency for the amount when bidding/buying versus
+	// asking/selling.
 	OrderSellingAmountsRequired bool
-	// FeeAppliedToSellingCurrency refers to an exchange that applies the fee to
-	// the selling currency. E.g. when buying 1 BTC with 100 USDT, the fee is
-	// applied to the 100 USDT. You will receive the entire amount of BTC.
+	// FeeAppliedToSellingCurrency indicates whether the exchange applies the
+	// fee to the selling currency. For example, when buying 1 BTC with 100 USDT,
+	// the fee is applied to the 100 USDT, and you receive  the entire amount of
+	// BTC.
 	FeeAppliedToSellingCurrency bool
-	// FeeAppliedToPurchasedCurrency refers to an exchange that applies the fee
-	// to the purchased currency. E.g. when buying 1 BTC with 100 USDT, the fee
-	// is applied to the 1 BTC. You will receive the entire amount of BTC minus
-	// the fee.
+	// FeeAppliedToPurchasedCurrency indicates whether the exchange applies the
+	// fee to the purchased currency. For example, when buying 1 BTC with 100
+	// USDT, the fee is applied to the 1 BTC, and you receive the entire amount
+	// of BTC minus the fee.
 	FeeAppliedToPurchasedCurrency bool
-	// FeePostOrderRequiresPrecisionOnAmount refers to when an exchange performs
-	// its fee calculation after they convert the amount to the precision. Thus
-	// putting is in their favour. e.g.
-	// BTC-USDT = $26359.28
-	// TradingFee - 0.1%
-	// Selling 5 USDT at that rate would give you ~0.00018968651647541208 BTC
-	// If true the exchange would convert the amount to precision first 0.000189
-	// then calculate the fee 0.000189 - 0.1% = 0.000188811
-	// Which would give you the balance of 0.000188811 BTC or 0.000188 BTC (front end precision).
-	// If false the exchange would calculate the fee first 0.00018968651647541208 - 0.1% =
-	// 0.00018949782782306367
-	// Which would give you the balance of 0.00018949782782306367 BTC or 0.000189 BTC (front end precision).
+	// FeePostOrderRequiresPrecisionOnAmount indicates whether the exchange
+	// performs its fee calculation after converting the amount to the precision.
+	// If true, the exchange converts the amount to precision first and then
+	// calculates the fee. If false, the exchange calculates the fee first and
+	// then converts the amount to precision. The precise behavior depends on
+	// the exchange's implementation.
 	FeePostOrderRequiresPrecisionOnAmount bool
-	// RequiresParameterLimits refers to an exchange that requires
-	// exchange parameters to be confined to precision and amount guidelines for
-	// order submission.
+	// RequiresParameterLimits indicates whether the exchange requires exchange
+	// parameters to be confinedto precision and amount guidelines for order
+	// submission.
 	RequiresParameterLimits bool
 }
-
-// func NewOrderBuilder() *Builder {
-// 	return &Builder{}
-// }
