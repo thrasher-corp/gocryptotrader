0d06a991833d3046baa3e5705b4fbffc578586b7
Romano <6548898+romanornr@users.noreply.github.com>
2023-06-13 22:41:24 +0200

    Update exchanges/fill/fill_test.go
    Co-authored-by: Scott <gloriousCode@users.noreply.github.com>

--- exchanges/fill/fill_test.go
+++ exchanges/fill/fill_test.go
@@ -29,10 +29,6 @@ func TestUpdateDisabledFeed(t *testing.T) {
 	// Send a test data to the Update function
 	testData := Data{Timestamp: time.Now(), Price: 15.2, Amount: 3.2}
 	err := fill.Update(testData)
-
-	//if err != ErrFeedDisabled {
-	//	t.Errorf("Expected ErrFeedDisabled, got %v", err)
-	//}
 	if !errors.Is(err, ErrFeedDisabled) {
 		t.Errorf("Expected ErrFeedDisabled, got %v", err)
 	}
