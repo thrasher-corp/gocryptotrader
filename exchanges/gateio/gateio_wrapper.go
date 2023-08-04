43a8044b44844d3527b91568f94f73b24be40545
Ryan O'Hara-Reid <oharareid.ryan@gmail.com>
2023-08-04 17:39:38 +1000

    gateio: websocket updates (#1282)
    * gateio: websocket updates
    
    * Update exchanges/gateio/gateio_websocket.go
    
    Co-authored-by: Scott <gloriousCode@users.noreply.github.com>
    
    * glorious: nits
    
    * revert that trick
    
    * glorious:nits
    
    * Update exchanges/gateio/gateio_ws_futures.go
    
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>
    
    * Update exchanges/gateio/gateio_ws_futures.go
    
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>
    
    * Update exchanges/gateio/gateio_websocket.go
    
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>
    
    * Update exchanges/gateio/gateio_websocket.go
    
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>
    
    * Update exchanges/gateio/gateio_ws_option.go
    
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>
    
    * Update exchanges/gateio/gateio_ws_option.go
    
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>
    
    * Update exchanges/gateio/gateio_websocket.go
    
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>
    
    * Update exchanges/gateio/gateio_websocket.go
    
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>
    
    * thrasher-nits
    
    * Update exchanges/gateio/gateio_ws_futures.go
    
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>
    
    * Update exchanges/gateio/gateio_ws_futures.go
    
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>
    
    * thrasher: nits rides again chapter 2 volume 1
    
    * rm unmarshaljson method for orderbook
    
    * use gateio time where it can and update tests
    
    * math.trunc and lower time frame on big books
    
    * :eagle
    
    ---------
    
    Co-authored-by: Ryan O'Hara-Reid <ryan.oharareid@thrasher.io>
    Co-authored-by: Scott <gloriousCode@users.noreply.github.com>
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>

--- exchanges/gateio/gateio_wrapper.go
+++ exchanges/gateio/gateio_wrapper.go
@@ -331,20 +331,16 @@ func (g *Gateio) UpdateTicker(ctx context.Context, p currency.Pair, a asset.Item
 			return nil, err
 		}
 		for x := range tickers {
-			if tickers[x].Name != fPair.String() {
+			if !tickers[x].Name.Equal(fPair) {
 				continue
 			}
-			var cp currency.Pair
-			cp, err = currency.NewPairFromString(strings.ReplaceAll(tickers[x].Name, currency.DashDelimiter, currency.UnderscoreDelimiter))
-			if err != nil {
-				return nil, err
-			}
-			cp.Quote = currency.NewCode(strings.ReplaceAll(cp.Quote.String(), currency.UnderscoreDelimiter, currency.DashDelimiter))
+			cleanQuote := strings.ReplaceAll(tickers[x].Name.Quote.String(), currency.UnderscoreDelimiter, currency.DashDelimiter)
+			tickers[x].Name.Quote = currency.NewCode(cleanQuote)
 			if err != nil {
 				return nil, err
 			}
 			tickerData = &ticker.Price{
-				Pair:         cp,
+				Pair:         tickers[x].Name,
 				Last:         tickers[x].LastPrice.Float64(),
 				Bid:          tickers[x].Bid1Price,
 				Ask:          tickers[x].Ask1Price,
@@ -644,17 +640,13 @@ func (g *Gateio) UpdateTickers(ctx context.Context, a asset.Item) error {
 				return err
 			}
 			for x := range tickers {
-				currencyPair, err := currency.NewPairFromString(tickers[x].Name)
-				if err != nil {
-					return err
-				}
 				err = ticker.ProcessTicker(&ticker.Price{
 					Last:         tickers[x].LastPrice.Float64(),
 					Ask:          tickers[x].Ask1Price,
 					AskSize:      tickers[x].Ask1Size,
 					Bid:          tickers[x].Bid1Price,
 					BidSize:      tickers[x].Bid1Size,
-					Pair:         currencyPair,
+					Pair:         tickers[x].Name,
 					ExchangeName: g.Name,
 					AssetType:    a,
 				})
@@ -724,20 +716,20 @@ func (g *Gateio) UpdateOrderbook(ctx context.Context, p currency.Pair, a asset.I
 		VerifyOrderbook: g.CanVerifyOrderbook,
 		Pair:            p.Upper(),
 		LastUpdateID:    orderbookNew.ID,
-		LastUpdated:     orderbookNew.Update,
+		LastUpdated:     orderbookNew.Update.Time(),
 	}
 	book.Bids = make(orderbook.Items, len(orderbookNew.Bids))
 	for x := range orderbookNew.Bids {
 		book.Bids[x] = orderbook.Item{
 			Amount: orderbookNew.Bids[x].Amount,
-			Price:  orderbookNew.Bids[x].Price,
+			Price:  orderbookNew.Bids[x].Price.Float64(),
 		}
 	}
 	book.Asks = make(orderbook.Items, len(orderbookNew.Asks))
 	for x := range orderbookNew.Asks {
 		book.Asks[x] = orderbook.Item{
 			Amount: orderbookNew.Asks[x].Amount,
-			Price:  orderbookNew.Asks[x].Price,
+			Price:  orderbookNew.Asks[x].Price.Float64(),
 		}
 	}
 	err = book.Process()
