c2f4217f42ca884376f57089e82e738c899ae50e
Adrian Gallagher <adrian.gallagher@thrasher.io>
2023-08-07 16:24:46 +1000

    Update engine/sync_manager.go

--- engine/sync_manager.go
+++ engine/sync_manager.go
@@ -330,7 +330,7 @@ func (m *syncManager) add(k currencyPairKey, s syncBase) *currencyPairSyncAgent 
 	return c
 }
 
-// Update notifies the syncManager to change the last updated time for a exchange asset pair
+// WebsocketUpdate notifies the syncManager to change the last updated time for a exchange asset pair
 // And set IsUsingWebsocket to true. It should be used externally only from websocket updaters
 func (m *syncManager) WebsocketUpdate(exchangeName string, p currency.Pair, a asset.Item, syncType syncItemType, err error) error {
 	if m == nil {
