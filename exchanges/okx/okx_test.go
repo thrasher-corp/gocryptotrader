843050980744f4b7f572f5abcd412c921fe783da
Gareth Kirwan <gbjkirwan@gmail.com>
2023-10-13 06:18:55 +0200

    Okx: Websocket order channel fixes (#1346)
    * Okx: Fix WS order fields
    
    * Fixes float64 with string annotation erroring on empty strings:
    Okx Order Push Data error json: invalid use of ,string struct tag, trying to unmarshal "" into float64
    Specifically this came from px field from a market order
    * Switch to convert.StringToFloat64 instead of okxNumericalValue
    * Fix typo in Notional* field names; Ironically prevented them from erroring
    
    * Okx: Add tests for first order fields
    
    * Okx: CID and maybe set WS order Filled time
    
    * Tests: Set TestFixtureToDataHandler to t.Helper
    
    * Orders: Add UnmarshalJSON to order.Side
    
    * Okx: Fix FillTime not parsed for PendingOrder
    
    * Okx: Switch to order.Side Unmarshal throughout
    
    * Okx: Add Fee and FeeAsset to order processing
    
    * Okx: Fix WS order.Detail amounts and Test
    
    This fixes Amount vs QuoteAmount for market sells where tgtCcy is
    quote_ccy
    
    * Add comment to order.Side.UnmarshalJSON
    
    * Okx: Replace PendingOrderItem Unmarshal with local types
    
    * Okx: string type for WS order reduceOnly
    
    Note: Not yet in unit tests, since it's not part of the spot tests I was
    originally fixing. I'll circle back to adding full test support for
    Reduce only and deleveraging positions.
    
    * Okx: Fix TestOrderPushData Amount
    
    We were expecting 0 when we're given a quoteAmount
    In reality, we'll calculate the size from the price
    
    * Okx: Fix order and remAmount in wsOrders
    
    Improved handling for Float64 issues and boundaries when the order is
    fully executed but not yet marked as Filled
    
    * Fix ErrSideIsInvalid in tests

--- exchanges/okx/okx_test.go
+++ exchanges/okx/okx_test.go
@@ -2490,13 +2490,70 @@ func TestBalanceAndPosition(t *testing.T) {
 	}
 }
 
-const orderPushDataJSON = `{"arg": {    "channel": "orders",    "instType": "SPOT",    "instId": "BTC-USDT",    "uid": "614488474791936"},"data": [    {        "accFillSz": "0.001",        "amendResult": "",        "avgPx": "31527.1",        "cTime": "1654084334977",        "category": "normal",        "ccy": "",        "clOrdId": "",        "code": "0",        "execType": "M",        "fee": "-0.02522168",        "feeCcy": "USDT",        "fillFee": "-0.02522168",        "fillFeeCcy": "USDT",        "fillNotionalUsd": "31.50818374",        "fillPx": "31527.1",        "fillSz": "0.001",        "fillTime": "1654084353263",        "instId": "BTC-USDT",        "instType": "SPOT",        "lever": "0",        "msg": "",        "notionalUsd": "31.50818374",        "ordId": "452197707845865472",        "ordType": "limit",        "pnl": "0",        "posSide": "",        "px": "31527.1",        "rebate": "0",        "rebateCcy": "BTC",        "reduceOnly": "false",        "reqId": "",        "side": "sell",        "slOrdPx": "",        "slTriggerPx": "",        "slTriggerPxType": "last",        "source": "",        "state": "filled",        "sz": "0.001",        "tag": "",        "tdMode": "cash",        "tgtCcy": "",        "tpOrdPx": "",        "tpTriggerPx": "",        "tpTriggerPxType": "last",        "tradeId": "242589207",        "uTime": "1654084353264"    }]}`
-
 func TestOrderPushData(t *testing.T) {
 	t.Parallel()
-	if err := ok.WsHandleData([]byte(orderPushDataJSON)); err != nil {
-		t.Error("Okx Order Push Data error", err)
+	n := new(Okx)
+	sharedtestvalues.TestFixtureToDataHandler(t, ok, n, "testdata/wsOrders.json", n.WsHandleData)
+	seen := 0
+	for reading := true; reading; {
+		select {
+		default:
+			reading = false
+		case resp := <-n.GetBase().Websocket.DataHandler:
+			seen++
+			switch v := resp.(type) {
+			case *order.Detail:
+				switch seen {
+				case 1:
+					assert.Equal(t, "452197707845865472", v.OrderID, "OrderID")
+					assert.Equal(t, "HamsterParty14", v.ClientOrderID, "ClientOrderID")
+					assert.Equal(t, asset.Spot, v.AssetType, "AssetType")
+					assert.Equal(t, order.Sell, v.Side, "Side")
+					assert.Equal(t, order.Filled, v.Status, "Status")
+					assert.Equal(t, order.Limit, v.Type, "Type")
+					assert.Equal(t, currency.NewPairWithDelimiter("BTC", "USDT", "-"), v.Pair, "Pair")
+					assert.Equal(t, 31527.1, v.AverageExecutedPrice, "AverageExecutedPrice")
+					assert.Equal(t, time.UnixMilli(1654084334977), v.Date, "Date")
+					assert.Equal(t, time.UnixMilli(1654084353263), v.CloseTime, "CloseTime")
+					assert.Equal(t, 0.001, v.Amount, "Amount")
+					assert.Equal(t, 0.001, v.ExecutedAmount, "ExecutedAmount")
+					assert.Equal(t, 0.000, v.RemainingAmount, "RemainingAmount")
+					assert.Equal(t, 31527.1, v.Price, "Price")
+					assert.Equal(t, 0.02522168, v.Fee, "Fee")
+					assert.Equal(t, currency.USDT, v.FeeAsset, "FeeAsset")
+				case 2:
+					assert.Equal(t, "620258920632008725", v.OrderID, "OrderID")
+					assert.Equal(t, asset.Spot, v.AssetType, "AssetType")
+					assert.Equal(t, order.Market, v.Type, "Type")
+					assert.Equal(t, order.Sell, v.Side, "Side")
+					assert.Equal(t, order.Active, v.Status, "Status")
+					assert.Equal(t, 0.0, v.Amount, "Amount should be 0 for a market sell")
+					assert.Equal(t, 10.0, v.QuoteAmount, "QuoteAmount")
+				case 3:
+					assert.Equal(t, "620258920632008725", v.OrderID, "OrderID")
+					assert.Equal(t, 10.0, v.QuoteAmount, "QuoteAmount")
+					assert.Equal(t, 0.00038127046945832905, v.Amount, "Amount")
+					assert.Equal(t, 0.010000249968, v.Fee, "Fee")
+					assert.Equal(t, 0.0, v.RemainingAmount, "RemainingAmount")
+					assert.Equal(t, 0.00038128, v.ExecutedAmount, "ExecutedAmount")
+					assert.Equal(t, order.PartiallyFilled, v.Status, "Status")
+				case 4:
+					assert.Equal(t, "620258920632008725", v.OrderID, "OrderID")
+					assert.Equal(t, 10.0, v.QuoteAmount, "QuoteAmount")
+					assert.Equal(t, 0.010000249968, v.Fee, "Fee")
+					assert.Equal(t, 0.0, v.RemainingAmount, "RemainingAmount")
+					assert.Equal(t, 0.00038128, v.ExecutedAmount, "ExecutedAmount")
+					assert.Equal(t, 0.00038128, v.Amount, "Amount should be derived because order filled")
+					assert.Equal(t, order.Filled, v.Status, "Status")
+				}
+			case error:
+				t.Error(v)
+			default:
+				t.Errorf("Got unexpected data: %T %v", v, v)
+			}
+		}
 	}
+	assert.Equal(t, 4, seen, "Saw 4 records")
 }
 
 const algoOrdersPushDataJSON = `{"arg": {"channel": "orders-algo","uid": "77982378738415879","instType": "FUTURES","instId": "BTC-USD-200329"},"data": [{"instType": "FUTURES","instId": "BTC-USD-200329","ordId": "312269865356374016","ccy": "BTC","algoId": "1234","px": "999","sz": "3","tdMode": "cross","tgtCcy": "","notionalUsd": "","ordType": "trigger","side": "buy","posSide": "long","state": "live","lever": "20","tpTriggerPx": "","tpTriggerPxType": "","tpOrdPx": "","slTriggerPx": "","slTriggerPxType": "","triggerPx": "99","triggerPxType": "last","ordPx": "12","actualSz": "","actualPx": "","tag": "adadadadad","actualSide": "","triggerTime": "1597026383085","cTime": "1597026383000"}]}`
