88182ec41400fad1a686981797b7c3828acbe204
Gareth Kirwan <gbjkirwan@gmail.com>
2023-11-24 03:50:01 +0100

    Engine: Remove Default Forex Provider (#1395)
    * Currency: Do not use a default forex provider
    
    exchangerate.host now requires an API key.
    Instead of finding a new Free (for now) default, this change simply
    disables the currency exchange when nothing is enabled.
    
    * SyncManager: Report ?.?? for an unknown forex amount
    
    In a situation where we thought forex was available but we got an error,
    this avoids showing 0.00 when there was actually an error.
    
    * Currency: Tests for no default forex
    
    * Currency: Use mock provider for tests
    
    * Currency: Add API key to exchangerate.host
    
    * Currency: Remove Exchangerate.host
    
    Exchangerate.host was bought by apilayer, the old API deprecated, and
    replaced with a proxy to the apilayer api.
    We already have currencylayer support, so ther's no reason to keep exh.
    
    Worth noting: New ERH keys actually work on currencylayer
    
    * Currencies: Add test coverage for currency layer
    
    * fixup! Currency: Tests for no default forex
    
    Remove duplicate assignment
    
    Fixes [review comment](https://github.com/thrasher-corp/gocryptotrader/pull/1395#discussion_r1395178513)
    
    * fixup! Currency: Add API key to exchangerate.host
    
    Remove unused ErrVar
    
    Fixes [review
    comment](https://github.com/thrasher-corp/gocryptotrader/pull/1395#discussion_r1396647418)
    
    * fixup! Currency: Tests for no default forex
    
    Fix spelling of override in test
    
    Fixes [review comment](https://github.com/thrasher-corp/gocryptotrader/pull/1395#discussion_r1396701476)
    
    * fixup! SyncManager: Report ?.?? for an unknown forex amount
    
    Fix display of non-positive currency conversions.
    
    Fixes [review comment](https://github.com/thrasher-corp/gocryptotrader/pull/1395/files#r1398527134)

--- engine/engine.go
+++ engine/engine.go
@@ -173,7 +173,6 @@ func validateSettings(b *Engine, s *Settings, flagSet FlagSet) {
 	flagSet.WithBool("exchangerates", &b.Settings.EnableExchangeRates, b.Config.Currency.ForexProviders.IsEnabled("exchangerates"))
 	flagSet.WithBool("fixer", &b.Settings.EnableFixer, b.Config.Currency.ForexProviders.IsEnabled("fixer"))
 	flagSet.WithBool("openexchangerates", &b.Settings.EnableOpenExchangeRates, b.Config.Currency.ForexProviders.IsEnabled("openexchangerates"))
-	flagSet.WithBool("exchangeratehost", &b.Settings.EnableExchangeRateHost, b.Config.Currency.ForexProviders.IsEnabled("exchangeratehost"))
 
 	flagSet.WithBool("datahistorymanager", &b.Settings.EnableDataHistoryManager, b.Config.DataHistoryManager.Enabled)
 	flagSet.WithBool("currencystatemanager", &b.Settings.EnableCurrencyStateManager, b.Config.CurrencyStateManager.Enabled != nil && *b.Config.CurrencyStateManager.Enabled)
@@ -403,12 +402,11 @@ func (bot *Engine) Start() error {
 			ExchangeRates:     bot.Settings.EnableExchangeRates,
 			Fixer:             bot.Settings.EnableFixer,
 			OpenExchangeRates: bot.Settings.EnableOpenExchangeRates,
-			ExchangeRateHost:  bot.Settings.EnableExchangeRateHost,
 		},
 		&bot.Config.Currency,
 		bot.Settings.DataDir,
 	); err != nil {
-		gctlog.Errorf(gctlog.Global, "ExchangeSettings updater system failed to start %s", err)
+		gctlog.Errorf(gctlog.Global, "Currency Converter system failed to start %s", err)
 	}
 
 	if bot.Settings.EnableGRPC {
@@ -677,7 +675,7 @@ func (bot *Engine) Stop() {
 
 	err = currency.ShutdownStorageUpdater()
 	if err != nil {
-		gctlog.Errorf(gctlog.Global, "ExchangeSettings storage system. Error: %v", err)
+		gctlog.Errorf(gctlog.Global, "Currency Converter unable to stop. Error: %v", err)
 	}
 
 	if !bot.Settings.EnableDryRun {
