3290edc872c6dc733d46e090c25db452b264f10c
Samuael A <39623015+samuael@users.noreply.github.com>
2024-01-18 01:12:56 +0300

    Enable proxy for websocket

--- exchanges/coinbaseinternational/coinbaseinternational_websocket.go
+++ exchanges/coinbaseinternational/coinbaseinternational_websocket.go
@@ -48,6 +48,8 @@ func (co *CoinbaseInternational) WsConnect() error {
 		return errors.New(stream.WebsocketNotEnabled)
 	}
 	var dialer websocket.Dialer
+	dialer.HandshakeTimeout = co.Config.HTTPTimeout
+	dialer.Proxy = http.ProxyFromEnvironment
 	err := co.Websocket.Conn.Dial(&dialer, http.Header{})
 	if err != nil {
 		return err
