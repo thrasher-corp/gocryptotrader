0aa741c71c39ac7a1e04ac9d74f256018107e2ae
Ryan O'Hara-Reid <ryan.oharareid@thrasher.io>
2024-02-22 09:17:34 +1100

    Add all protection back in and jankyness because races. :'(

--- common/timedmutex/timed_mutex.go
+++ common/timedmutex/timed_mutex.go
@@ -2,42 +2,54 @@ package timedmutex
 
 import (
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
-// TimedMutex is a blocking mutex which will unlock
-// after a specified time
+// TimedMutex is a blocking mutex which will unlock after a specified time
 type TimedMutex struct {
-	mtx      sync.Mutex
-	timer    *time.Timer
-	duration time.Duration
+	primary   sync.Mutex
+	secondary sync.Mutex
+	timer     *time.Timer
+	primed    atomic.Bool
+	duration  time.Duration
 }
 
-// NewTimedMutex creates a new timed mutex with a
-// specified duration
+// NewTimedMutex creates a new timed mutex with a specified duration
 func NewTimedMutex(length time.Duration) *TimedMutex { return &TimedMutex{duration: length} }
 
-// LockForDuration will start a timer, lock the mutex,
-// then allow the caller to continue
+// LockForDuration will start a timer, lock the mutex, then allow the caller to continue
 // After the duration, the mutex will be unlocked
 func (t *TimedMutex) LockForDuration() {
-	t.mtx.Lock()
-	if t.timer == nil {
-		t.timer = time.AfterFunc(t.duration, func() { t.mtx.Unlock() })
+	t.primary.Lock()
+	if t.primed.CompareAndSwap(false, true) {
+		t.secondary.Lock()
+		t.timer = time.AfterFunc(t.duration, func() { t.primary.Unlock() })
+		t.secondary.Unlock()
 	} else {
 		// Timer C channel is not used with AfterFunc, so no need to drain.
+		t.secondary.Lock()
 		t.timer.Reset(t.duration)
+		t.secondary.Unlock()
 	}
 }
 
-// UnlockIfLocked will unlock the mutex if its currently locked
-// Will return true if successfully unlocked
+// UnlockIfLocked will unlock the mutex if its currently locked Will return true
+// if successfully unlocked
 func (t *TimedMutex) UnlockIfLocked() bool {
-	if t.timer == nil || !t.timer.Stop() {
+	if !t.primed.Load() {
+		return false
+	}
+
+	t.secondary.Lock()
+	hasStopped := t.timer.Stop()
+	t.secondary.Unlock()
+
+	if !hasStopped {
 		// Timer has already fired and the mutex has been unlocked.
 		// Timer C channel is not used with AfterFunc, so no need to drain.
 		return false
 	}
-	t.mtx.Unlock()
+	t.primary.Unlock()
 	return true
 }
