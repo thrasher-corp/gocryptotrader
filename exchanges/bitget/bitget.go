eb73fa6602f2393749be19c5729be7914aac9429
Samuel Reid <43227667+cranktakular@users.noreply.github.com>
2024-03-08 12:15:29 +1100

    Bitget progress

--- exchanges/bitget/bitget.go
+++ exchanges/bitget/bitget.go
@@ -1,6 +1,14 @@
 package bitget
 
 import (
+	"context"
+	"encoding/json"
+	"net/url"
+	"strconv"
+	"time"
+
+	"github.com/thrasher-corp/gocryptotrader/common"
+	"github.com/thrasher-corp/gocryptotrader/common/crypto"
 	exchange "github.com/thrasher-corp/gocryptotrader/exchanges"
 )
 
@@ -10,17 +18,45 @@ type Bitget struct {
 }
 
 const (
-	bitgetAPIURL     = ""
-	bitgetAPIVersion = ""
+	bitgetAPIURL = "api.bitget.com"
 
 	// Public endpoints
 
 	// Authenticated endpoints
+
+	// Errors
+	errUnknownEndpointLimit = "unknown endpoint limit %v"
 )
 
 // Start implementing public and private exchange API funcs below
 
-func (b *Bitget) SendAuthenticatedHTTPRequest() error {
-
+func (b *Bitget) SendAuthenticatedHTTPRequest(ctx context.Context, method, path string, queryparams url.Values, bodyParams map[string]interface{}) error {
+	creds, err := b.GetCredentials(ctx)
+	if err != nil {
+		return err
+	}
+	payload := []byte("")
+	if bodyParams != nil {
+		payload, err = json.Marshal(bodyParams)
+		if err != nil {
+			return err
+		}
+	}
+	query := common.EncodeURLValues("", queryparams)
+	t := strconv.FormatInt(time.Now().UnixMilli(), 10)
+	message := t + method + path + query + string(payload)
+	// The exchange also supports user-generated RSA keys, but we haven't implemented that yet
+	var hmac []byte
+	hmac, err = crypto.GetHMAC(crypto.HashSHA256, []byte(message), []byte(creds.Secret))
+	if err != nil {
+		return err
+	}
+	headers := make(map[string]string)
+	headers["ACCESS-KEY"] = creds.Key
+	headers["ACCESS-SIGN"] = crypto.Base64Encode(hmac)
+	headers["ACCESS-TIMESTAMP"] = t
+	headers["ACCESS-PASSPHRASE"] = creds.ClientID
+	headers["Content-Type"] = "application/json"
+	headers["locale"] = "en-US"
 	return nil
 }
--- exchanges/bitget/ratelimit.go
+++ exchanges/bitget/ratelimit.go
@@ -4,29 +4,64 @@ import (
 	"context"
 	"time"
 
+	"github.com/pkg/errors"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/request"
 	"golang.org/x/time/rate"
 )
 
-// Coinbasepro rate limit conts
+// Coinbasepro rate limit constants
 const (
 	bitgetRateInterval = time.Second
-	bitgetRate         = 10
+	bitgetRate20       = 20
+	bitgetRate10       = 10
+	bitgetRate5        = 5
+	bitgetRate2        = 2
+	bitgetRate1        = 1
+)
+
+// Bitget rate limits
+const (
+	Rate20 request.EndpointLimit = iota
+	Rate10
+	Rate5
+	Rate2
+	Rate1
 )
 
 // RateLimit implements the request.Limiter interface
 type RateLimit struct {
-	Default *rate.Limiter
+	RateLim20 *rate.Limiter
+	RateLim10 *rate.Limiter
+	RateLim5  *rate.Limiter
+	RateLim2  *rate.Limiter
+	RateLim1  *rate.Limiter
 }
 
 // Limit limits outbound calls
 func (r *RateLimit) Limit(ctx context.Context, f request.EndpointLimit) error {
-	return r.Default.Wait(ctx)
+	switch f {
+	case Rate20:
+		return r.RateLim20.Wait(ctx)
+	case Rate10:
+		return r.RateLim10.Wait(ctx)
+	case Rate5:
+		return r.RateLim5.Wait(ctx)
+	case Rate2:
+		return r.RateLim2.Wait(ctx)
+	case Rate1:
+		return r.RateLim1.Wait(ctx)
+	default:
+		return errors.Errorf(errUnknownEndpointLimit, f)
+	}
 }
 
 // SetRateLimit returns the rate limit for the exchange
 func SetRateLimit() *RateLimit {
 	return &RateLimit{
-		Default: request.NewRateLimit(bitgetRateInterval, bitgetRate),
+		RateLim20: request.NewRateLimit(bitgetRateInterval, bitgetRate20),
+		RateLim10: request.NewRateLimit(bitgetRateInterval, bitgetRate10),
+		RateLim5:  request.NewRateLimit(bitgetRateInterval, bitgetRate5),
+		RateLim2:  request.NewRateLimit(bitgetRateInterval, bitgetRate2),
+		RateLim1:  request.NewRateLimit(bitgetRateInterval, bitgetRate1),
 	}
 }
