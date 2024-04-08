ceb3aa04407569f3615043b34a597265b790a106
Gareth Kirwan <gbjkirwan@gmail.com>
2024-04-08 10:01:55 +0700

    Subscriptions: Add List.GroupPairs Rename sub.AddPairs

--- exchanges/subscription/list.go
+++ exchanges/subscription/list.go
@@ -16,3 +16,23 @@ func (l List) Strings() []string {
 	slices.Sort(s)
 	return s
 }
+
+// GroupPairs groups subscriptions which are identical apart from the Pairs
+// The returned List contains cloned Subscriptions, and the original Subscriptions are left alone
+func (l List) GroupPairs() (n List) {
+	src := slices.Clone(l)
+	for len(src) > 0 {
+		s := src[0].Clone()
+		key := &IgnoringPairsKey{s}
+		n = append(n, s)
+		// Remove the first element, and any which match it
+		src = slices.DeleteFunc(src[1:], func(eachSub *Subscription) bool {
+			if key.Match(&IgnoringPairsKey{eachSub}) {
+				s.AddPairs(eachSub.Pairs...)
+				return true
+			}
+			return false
+		})
+	}
+	return
+}
