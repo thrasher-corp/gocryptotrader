01802ec90b7a5d33eae70b8b0e483f65c71a16c2
Gareth Kirwan <gbjkirwan@gmail.com>
2024-04-09 14:43:11 +0700

    Subscriptions: Remove unused IdentityKey and HasPairKey

--- exchanges/subscription/keys.go
+++ exchanges/subscription/keys.go
@@ -7,34 +7,26 @@ type MatchableKey interface {
 	GetSubscription() *Subscription
 }
 
-// HasPairKey is a key type for subscriptions where a subset of the pairs may match
-// With empty pairs then only a sub without pairs will match
-// Otherwise a sub which contains all the pairs will match
-// Most likely usage is searching for a subscriptions to many pairs given just one of those pairs
-type HasPairKey struct {
+// ExactKey is key type for subscriptions where all the pairs in a Subscription must match exactly
+type ExactKey struct {
 	*Subscription
 }
 
-var _ MatchableKey = HasPairKey{} // Enforce HasPairKey must implement MatchableKey
+var _ MatchableKey = ExactKey{} // Enforce ExactKey must implement MatchableKey
 
 // GetSubscription returns the underlying subscription
-func (k HasPairKey) GetSubscription() *Subscription {
+func (k ExactKey) GetSubscription() *Subscription {
 	return k.Subscription
 }
 
 // Match implements MatchableKey
-// With empty pairs then only a sub without pairs will match
-// Otherwise a sub which contains all the pairs will match
-// Most likely usage is searching for a subscriptions to many pairs given just one of those pairs
-func (k HasPairKey) Match(eachKey MatchableKey) bool {
+// Returns true if the key fields exactly matches the subscription, including all Pairs
+func (k ExactKey) Match(eachKey MatchableKey) bool {
 	eachSub := eachKey.GetSubscription()
 	switch {
 	case eachSub.Channel != k.Channel,
 		eachSub.Asset != k.Asset,
-		// len(eachSub.Pairs) == 0 && len(s.Pairs) == 0: Okay; continue to next non-pairs check
-		len(eachSub.Pairs) == 0 && len(k.Pairs) != 0,
-		len(eachSub.Pairs) != 0 && len(k.Pairs) == 0,
-		len(k.Pairs) != 0 && eachSub.Pairs.ContainsAll(k.Pairs, true) != nil,
+		!eachSub.Pairs.Equal(k.Pairs),
 		eachSub.Levels != k.Levels,
 		eachSub.Interval != k.Interval:
 		return false
@@ -67,52 +59,4 @@ func (k IgnoringPairsKey) Match(eachKey MatchableKey) bool {
 	}
 
 	return true
-}
-
-// ExactKey is key type for subscriptions where all the pairs in a Subscription must match exactly
-type ExactKey struct {
-	*Subscription
-}
-
-var _ MatchableKey = ExactKey{} // Enforce ExactKey must implement MatchableKey
-
-// GetSubscription returns the underlying subscription
-func (k ExactKey) GetSubscription() *Subscription {
-	return k.Subscription
-}
-
-// Match implements MatchableKey
-// Returns true if the key fields exactly matches the subscription, including all Pairs
-func (k ExactKey) Match(eachKey MatchableKey) bool {
-	eachSub := eachKey.GetSubscription()
-	switch {
-	case eachSub.Channel != k.Channel,
-		eachSub.Asset != k.Asset,
-		!eachSub.Pairs.Equal(k.Pairs),
-		eachSub.Levels != k.Levels,
-		eachSub.Interval != k.Interval:
-		return false
-	}
-
-	return true
-}
-
-// IdentityKey is key type for subscriptions where we know the exact sub we want to remove, so we use the pointer itself
-// This is useful during Unsubscribe, when you might have concurrently reduced 2 subscriptions to having no Pairs
-type IdentityKey struct {
-	*Subscription
-}
-
-var _ MatchableKey = IdentityKey{} // Enforce IdentityKey must implement MatchableKey
-
-// GetSubscription returns the underlying subscription
-func (k IdentityKey) GetSubscription() *Subscription {
-	return k.Subscription
-}
-
-// Match implements MatchableKey
-// Returns true if the key is the same pointer address
-func (k IdentityKey) Match(eachKey MatchableKey) bool {
-	eachSub := eachKey.GetSubscription()
-	return eachSub == k.Subscription
 }
