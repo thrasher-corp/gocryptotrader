d7121d245e92b7c1f9c042ad0b9aec66cac89359
Gareth Kirwan <gbjkirwan@gmail.com>
2024-04-09 12:08:33 +0700

    Subscription: Fix ExactKey not matching 0 pairs

--- exchanges/subscription/keys_test.go
+++ exchanges/subscription/keys_test.go
@@ -3,25 +3,40 @@ package subscription
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/thrasher-corp/gocryptotrader/currency"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/asset"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/kline"
 )
 
-// TestSubsetKeyMatch exercises the HasPairKey MatchableKey interface implementation
-// Given A.Match(B):
-// Where A is the incoming key, and B is each key in the store
-// Ensures A.Pairs must be a subset of B.Pairs
-func TestHasPairKeyMatch(t *testing.T) {
+// DummyKey is a test key type that ensures that cross compatible keys can be used
+// It will panic if Match() is called
+type DummyKey struct {
+	*Subscription
+	detonator testing.TB
+}
+
+var _ MatchableKey = DummyKey{} // Enforce DummyKey must implement MatchableKey
+
+// GetSubscription returns the underlying subscription
+func (k DummyKey) GetSubscription() *Subscription {
+	return k.Subscription
+}
+
+// Match implements MatchableKey
+func (k DummyKey) Match(_ MatchableKey) bool {
+	k.detonator.Fatal("DummyKey Match should never be called")
+	return false
+}
+
+// TestExactKeyMatch exercises the MatchableKey interface implementation
+func TestExactKeyMatch(t *testing.T) {
 	t.Parallel()
 
-	key := &HasPairKey{&Subscription{Channel: TickerChannel}}
-	try := &ExactKey{&Subscription{Channel: OrderbookChannel}}
+	key := &ExactKey{&Subscription{Channel: TickerChannel}}
+	try := &DummyKey{&Subscription{Channel: OrderbookChannel}, t}
 
-	assert.NotNil(t, key.EnsureKeyed(), "EnsureKeyed should work")
-
+	require.False(t, key.Match(nil), "Match on a nil must return false")
 	require.False(t, key.Match(try), "Gate 1: Match must reject a bad Channel")
 	try.Channel = TickerChannel
 	require.True(t, key.Match(try), "Gate 1: Match must accept a good Channel")
@@ -29,29 +44,50 @@ func TestHasPairKeyMatch(t *testing.T) {
 	require.False(t, key.Match(try), "Gate 2: Match must reject a bad Asset")
 	try.Asset = asset.Spot
 	require.True(t, key.Match(try), "Gate 2: Match must accept a good Asset")
-
 	key.Pairs = currency.Pairs{btcusdtPair}
 	require.False(t, key.Match(try), "Gate 3: Match must reject B empty Pairs when key has Pairs")
 	try.Pairs = currency.Pairs{btcusdtPair}
 	key.Pairs = nil
-	require.False(t, key.Match(try), "Gate 4: Match must reject B has Pairs when key has empty Pairs")
+	require.False(t, key.Match(try), "Gate 3: Match must reject B has Pairs when key has empty Pairs")
 	key.Pairs = currency.Pairs{btcusdtPair}
-	require.True(t, key.Match(try), "Gate 5: Match must accept matching pairs")
+	require.True(t, key.Match(try), "Gate 3: Match must accept matching pairs")
 	key.Pairs = currency.Pairs{ethusdcPair}
-	require.False(t, key.Match(try), "Gate 5: Match must reject when key.Pairs not in try.Pairs")
+	require.False(t, key.Match(try), "Gate 3: Match must reject when key.Pairs not matching")
 	try.Pairs = currency.Pairs{btcusdtPair, ethusdcPair}
-	require.True(t, key.Match(try), "Gate 5: Match must accept one of the key.Pairs in try.Pairs")
-	key.Pairs = currency.Pairs{btcusdtPair, ethusdcPair}
-	try.Pairs = currency.Pairs{btcusdtPair, ltcusdcPair}
-	require.False(t, key.Match(try), "Gate 5: Match must reject when key.Pairs not in try.Pairs")
-	try.Pairs = currency.Pairs{btcusdtPair, ethusdcPair, ltcusdcPair}
-	require.True(t, key.Match(try), "Gate 5: Match must accept when all key.Pairs are subset of try.Pairs")
+	require.False(t, key.Match(try), "Gate 3: Match must reject when key.Pairs is only a subset")
+	key.Pairs = currency.Pairs{ethusdcPair, btcusdtPair}
+	require.True(t, key.Match(try), "Gate 3: Match accept when Pairs match in different order")
 	key.Levels = 4
-	require.False(t, key.Match(try), "Gate 6: Match must reject a bad Level")
+	require.False(t, key.Match(try), "Gate 4: Match must reject a bad Level")
 	try.Levels = 4
-	require.True(t, key.Match(try), "Gate 6: Match must accept a good Level")
+	require.True(t, key.Match(try), "Gate 4: Match must accept a good Level")
 	key.Interval = kline.FiveMin
-	require.False(t, key.Match(try), "Gate 7: Match must reject a bad Interval")
+	require.False(t, key.Match(try), "Gate 5: Match must reject a bad Interval")
 	try.Interval = kline.FiveMin
-	require.True(t, key.Match(try), "Gate 7: Match must accept a good Interval")
+	require.True(t, key.Match(try), "Gate 5: Match must accept a good Interval")
+}
+
+// TestIgnoringPairsKeyMatch exercises the MatchableKey interface implementation
+func TestIgnoringPairsKeyMatch(t *testing.T) {
+	t.Parallel()
+
+	key := &IgnoringPairsKey{&Subscription{Channel: TickerChannel, Pairs: currency.Pairs{btcusdtPair}}}
+	try := &DummyKey{&Subscription{Channel: OrderbookChannel, Pairs: currency.Pairs{ethusdcPair}}, t}
+
+	require.False(t, key.Match(nil), "Match on a nil must return false")
+	require.False(t, key.Match(try), "Gate 1: Match must reject a bad Channel")
+	try.Channel = TickerChannel
+	require.True(t, key.Match(try), "Gate 1: Match must accept a good Channel")
+	key.Asset = asset.Spot
+	require.False(t, key.Match(try), "Gate 2: Match must reject a bad Asset")
+	try.Asset = asset.Spot
+	require.True(t, key.Match(try), "Gate 2: Match must accept a good Asset")
+	key.Levels = 4
+	require.False(t, key.Match(try), "Gate 3: Match must reject a bad Level")
+	try.Levels = 4
+	require.True(t, key.Match(try), "Gate 3: Match must accept a good Level")
+	key.Interval = kline.FiveMin
+	require.False(t, key.Match(try), "Gate 4: Match must reject a bad Interval")
+	try.Interval = kline.FiveMin
+	require.True(t, key.Match(try), "Gate 4: Match must accept a good Interval")
 }
