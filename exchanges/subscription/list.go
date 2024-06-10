0bc9f9cf9e1b08ebd1ca1f9a75599d1a2cb78e74
Gareth Kirwan <gbjkirwan@gmail.com>
2024-06-10 13:23:55 +0700

    Subscriptions: Add List.AssetPairs

--- exchanges/subscription/list.go
+++ exchanges/subscription/list.go
@@ -30,3 +30,48 @@ func (l List) GroupPairs() (n List) {
 	}
 	return s.List()
 }
+
+type assetPairs map[asset.Item]currency.Pairs
+type iExchange interface {
+	GetAssetTypes(enabled bool) asset.Items
+	GetEnabledPairs(asset.Item) (currency.Pairs, error)
+	GetPairFormat(asset.Item, bool) (currency.PairFormat, error)
+}
+
+func fillAssetPairs(ap assetPairs, a asset.Item, e iExchange) error {
+	p, err := e.GetEnabledPairs(a)
+	if err != nil {
+		return err
+	}
+	f, err := e.GetPairFormat(a, true)
+	if err != nil {
+		return err
+	}
+	ap[a] = p.Format(f)
+	return nil
+}
+
+// assetPairs returns a map of enabled pairs for the subscriptions in the list, formatted for the asset
+func (l List) assetPairs(e iExchange) (assetPairs, error) {
+	at := e.GetAssetTypes(true)
+	ap := assetPairs{}
+	for _, s := range l {
+		switch s.Asset {
+		case asset.Empty:
+			// Nothing to do
+		case asset.All:
+			for _, a := range at {
+				if err := fillAssetPairs(ap, a, e); err != nil {
+					return nil, err
+				}
+			}
+		default:
+			if slices.Contains(at, s.Asset) {
+				if err := fillAssetPairs(ap, s.Asset, e); err != nil {
+					return nil, err
+				}
+			}
+		}
+	}
+	return ap, nil
+}
