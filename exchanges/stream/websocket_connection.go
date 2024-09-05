9c21bd9f123638f8bdd858544fd08f03b4a30a9c
shazbert <ryan.oharareid@thrasher.io>
2024-09-05 12:03:58 +1000

    Add verbosity

--- exchanges/stream/websocket_connection.go
+++ exchanges/stream/websocket_connection.go
@@ -57,7 +57,7 @@ func (w *WebsocketConnection) Dial(dialer *websocket.Dialer, headers http.Header
 // SendJSONMessage sends a JSON encoded message over the connection
 func (w *WebsocketConnection) SendJSONMessage(ctx context.Context, data interface{}) error {
 	return w.writeToConn(ctx, func() error {
-		if w.Verbose {
+		if request.IsVerbose(ctx, w.Verbose) {
 			if msg, err := json.Marshal(data); err == nil { // WriteJSON will error for us anyway
 				log.Debugf(log.WebsocketMgr, "%s websocket connection: sending message: %s\n", w.ExchangeName, msg)
 			}
@@ -69,7 +69,7 @@ func (w *WebsocketConnection) SendJSONMessage(ctx context.Context, data interfac
 // SendRawMessage sends a message over the connection without JSON encoding it
 func (w *WebsocketConnection) SendRawMessage(ctx context.Context, messageType int, message []byte) error {
 	return w.writeToConn(ctx, func() error {
-		if w.Verbose {
+		if request.IsVerbose(ctx, w.Verbose) {
 			log.Debugf(log.WebsocketMgr, "%v websocket connection: sending message [%s]\n", w.ExchangeName, message)
 		}
 		return w.Connection.WriteMessage(messageType, message)
@@ -287,8 +287,8 @@ func (w *WebsocketConnection) SendMessageReturnResponse(ctx context.Context, sig
 
 // SendMessageReturnResponses will send a WS message to the connection and wait for N responses
 // An error of ErrSignatureTimeout can be ignored if individual responses are being otherwise tracked
-func (w *WebsocketConnection) SendMessageReturnResponses(ctx context.Context, signature, request any, expected int) ([][]byte, error) {
-	outbound, err := json.Marshal(request)
+func (w *WebsocketConnection) SendMessageReturnResponses(ctx context.Context, signature, payload any, expected int) ([][]byte, error) {
+	outbound, err := json.Marshal(payload)
 	if err != nil {
 		return nil, fmt.Errorf("error marshaling json for %s: %w", signature, err)
 	}
@@ -296,6 +296,10 @@ func (w *WebsocketConnection) SendMessageReturnResponses(ctx context.Context, si
 	ch, err := w.Match.Set(signature, expected)
 	if err != nil {
 		return nil, err
+	}
+
+	if request.IsVerbose(ctx, w.Verbose) {
+		log.Debugf(log.WebsocketMgr, "%v %v websocket connection: Sending message: %v", w.ExchangeName, w.URL, string(outbound))
 	}
 
 	start := time.Now()
@@ -324,6 +328,12 @@ func (w *WebsocketConnection) SendMessageReturnResponses(ctx context.Context, si
 
 	if err == nil && w.Reporter != nil {
 		w.Reporter.Latency(w.ExchangeName, outbound, time.Since(start))
+	}
+
+	if request.IsVerbose(ctx, w.Verbose) {
+		for i := range resps {
+			log.Debugf(log.WebsocketMgr, "%v %v websocket connection: Received response [%d out of %d]: %v", w.ExchangeName, w.URL, i+1, len(resps), string(resps[i]))
+		}
 	}
 
 	return resps, err
