b91e611526fedad66dd233ef956f546504b979dd
Gareth Kirwan <gbjkirwan@gmail.com>
2024-09-18 12:12:35 +0700

    fixup! Subscription: Add List.GroupByPairs

--- exchanges/subscription/list.go
+++ exchanges/subscription/list.go
@@ -52,7 +52,7 @@ func (l List) GroupByPairs() []List {
 outer:
 	for _, a := range l {
 		for i, b := range n {
-			if a.Pairs.Equal(b[0].Pairs) {
+			if a.Pairs.Equal(b[0].Pairs) { // Note: b is guaranteed to have 1 element by the append(n) below
 				n[i] = append(n[i], a)
 				continue outer
 			}
