827272837ab3fb79e5039a90eb5daaf2fd4c7668
Gareth Kirwan <gbjkirwan@gmail.com>
2024-10-12 16:33:25 +0700

    fixup! Config: Version Management

--- config/versions/testfixtures/v9997/fixture.go
+++ config/versions/testfixtures/v9997/fixture.go
@@ -8,12 +8,15 @@ import (
 type Version struct {
 }
 
+// Disabled implements the DisabledVersion interface
 func (v *Version) Disabled() {}
 
+// UpgradeConfig implements the ConfigdVersion interface
 func (v *Version) UpgradeConfig(_ context.Context, c []byte) ([]byte, error) {
 	return c, nil
 }
 
+// DowngradeConfig implements the ConfigdVersion interface
 func (v *Version) DowngradeConfig(_ context.Context, c []byte) ([]byte, error) {
 	return c, nil
 }
