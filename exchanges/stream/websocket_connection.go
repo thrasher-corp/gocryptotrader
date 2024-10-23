31339ed5155bc97ca9000e0d74818716fc617f56
Ryan O'Hara-Reid <ryan.oharareid@thrasher.io>
2024-10-23 11:49:45 +1100

    linter: fix again

--- exchanges/stream/websocket_connection.go
+++ exchanges/stream/websocket_connection.go
@@ -315,7 +315,8 @@ func (w *WebsocketConnection) SendMessageReturnResponses(ctx context.Context, ep
 	}
 
 	start := time.Now()
-	if err = w.SendRawMessage(ctx, epl, websocket.TextMessage, outbound); err != nil {
+	err = w.SendRawMessage(ctx, epl, websocket.TextMessage, outbound)
+	if err != nil {
 		return nil, err
 	}
 
