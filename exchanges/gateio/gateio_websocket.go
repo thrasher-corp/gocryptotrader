bfc8550f0c7101612c0dd800cce911fe7bcd27dd
Ryan O'Hara-Reid <ryan.oharareid@thrasher.io>
2024-11-18 15:18:54 +1100

    abstract adding and removing subscriptions

--- exchanges/gateio/gateio_websocket.go
+++ exchanges/gateio/gateio_websocket.go
@@ -667,10 +667,7 @@ func (g *Gateio) manageSubs(ctx context.Context, event string, conn stream.Conne
 			if resp.Error != nil && resp.Error.Code != 0 {
 				return fmt.Errorf("(%d) %s", resp.Error.Code, resp.Error.Message)
 			}
-			if event == "unsubscribe" {
-				return g.Websocket.RemoveSubscriptions(conn, s)
-			}
-			return g.Websocket.AddSuccessfulSubscriptions(conn, s)
+			return nil
 		}(); err != nil {
 			errs = common.AppendError(errs, fmt.Errorf("%s %s %s: %w", s.Channel, s.Asset, s.Pairs, err))
 		}
@@ -792,12 +789,6 @@ func (g *Gateio) handleSubscription(ctx context.Context, conn stream.Connection,
 		} else {
 			if resp.Error != nil && resp.Error.Code != 0 {
 				errs = common.AppendError(errs, fmt.Errorf("error while %s to channel %s error code: %d message: %s", payloads[k].Event, payloads[k].Channel, resp.Error.Code, resp.Error.Message))
-				continue
-			}
-			if event == subscribeEvent {
-				errs = common.AppendError(errs, g.Websocket.AddSuccessfulSubscriptions(conn, channelsToSubscribe[k]))
-			} else {
-				errs = common.AppendError(errs, g.Websocket.RemoveSubscriptions(conn, channelsToSubscribe[k]))
 			}
 		}
 	}
