789da794b775091a19ba3494a15b112ec5e5e413
shazbert <ryan.oharareid@thrasher.io>
2025-01-14 10:23:24 +1100

    Add config upgrade and downgrade

---
+++ config/versions/v3_test.go
@@ -0,0 +1,41 @@
+package versions
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestVersion3UpgradeExchange(t *testing.T) {
+	t.Parallel()
+
+	got, err := (&Version3{}).UpgradeExchange(nil, nil)
+	require.NoError(t, err)
+	require.Nil(t, got)
+
+	payload := []byte(`{"orderbook": {"verificationBypass": false,"websocketBufferLimit": 5,"websocketBufferEnabled": false,"publishPeriod": 10000000000}}`)
+	expected := []byte(`{"orderbook": {"verificationBypass": false,"websocketBufferLimit": 5,"websocketBufferEnabled": false}}`)
+	got, err = (&Version3{}).UpgradeExchange(nil, payload)
+	require.NoError(t, err)
+	require.Equal(t, got, expected)
+}
+
+func TestVersion3DowngradeExchange(t *testing.T) {
+	t.Parallel()
+
+	got, err := (&Version3{}).DowngradeExchange(nil, nil)
+	require.NoError(t, err)
+	require.Nil(t, got)
+
+	payload := []byte(`{"orderbook": {"verificationBypass": false,"websocketBufferLimit": 5,"websocketBufferEnabled": false}}`)
+	expected := []byte(`{"orderbook": {"verificationBypass": false,"websocketBufferLimit": 5,"websocketBufferEnabled": false,"publishPeriod":10000000000}}`)
+	got, err = (&Version3{}).DowngradeExchange(nil, payload)
+	require.NoError(t, err)
+	require.Equal(t, got, expected)
+}
+
+func TestVersion3Exchanges(t *testing.T) {
+	t.Parallel()
+	assert := require.New(t)
+	assert.Equal([]string{"*"}, (&Version3{}).Exchanges())
+}
