73da218c9a51d57ec56e9a54a25fb22e4061ba23
shazbert <ryan.oharareid@thrasher.io>
2025-01-14 16:56:32 +1100

    config version

---
+++ config/versions/v3_test.go
@@ -0,0 +1,47 @@
+package versions
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestVersion3UpgradeExchange(t *testing.T) {
+	t.Parallel()
+
+	got, err := (&Version3{}).UpgradeExchange(context.Background(), nil)
+	require.NoError(t, err)
+	require.Nil(t, got)
+
+	payload := []byte(`{"name":"test"}`)
+	expected := []byte(`{"name":"test","websocketMetricsLogging":false}`)
+	got, err = (&Version3{}).UpgradeExchange(context.Background(), payload)
+	require.NoError(t, err)
+	require.Equal(t, expected, got)
+
+	payload = []byte(`{"name":"test","websocketMetricsLogging":true}`)
+	got, err = (&Version3{}).UpgradeExchange(context.Background(), payload)
+	require.NoError(t, err)
+	require.Equal(t, payload, got)
+}
+
+func TestVersion3DowngradeExchange(t *testing.T) {
+	t.Parallel()
+
+	got, err := (&Version3{}).DowngradeExchange(context.Background(), nil)
+	require.NoError(t, err)
+	require.Nil(t, got)
+
+	payload := []byte(`{"name":"test","websocketMetricsLogging":false}`)
+	expected := []byte(`{"name":"test"}`)
+	got, err = (&Version3{}).DowngradeExchange(context.Background(), payload)
+	require.NoError(t, err)
+	require.Equal(t, expected, got)
+}
+
+func TestVersion3Exchanges(t *testing.T) {
+	t.Parallel()
+	assert := require.New(t)
+	assert.Equal([]string{"*"}, (&Version3{}).Exchanges())
+}
