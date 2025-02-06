9a72b9966c39d99c69f10f0b205eab90147c598a
Ryan O'Hara-Reid <oharareid.ryan@gmail.com>
2025-02-07 05:09:25 +1100

    Update config/versions/v3.go
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>

--- config/versions/v3.go
+++ config/versions/v3.go
@@ -27,7 +27,7 @@ func (v *Version3) UpgradeExchange(_ context.Context, e []byte) ([]byte, error) 
 
 var defaultOrderbookPublishPeriod = time.Second * 10
 
-// DowngradeExchange will downgrade the exchange config with the default orderbook publish period
+// DowngradeExchange will downgrade the exchange's config by setting the default orderbook publish period
 func (v *Version3) DowngradeExchange(_ context.Context, e []byte) ([]byte, error) {
 	if _, _, _, err := jsonparser.Get(e, "orderbook"); err != nil {
 		return e, nil //nolint:nilerr // No error, just return the original config
