7880d163a240688122ec3ae4dddb48270c1fb30c
Gareth Kirwan <gbjkirwan@gmail.com>
2025-02-17 08:57:53 +0700

    fixup! Subscriptions: Fix no enabled pairs for one asset

--- exchanges/subscription/list_test.go
+++ exchanges/subscription/list_test.go
@@ -118,23 +118,23 @@ func TestAssetPairs(t *testing.T) {
 func TestAssetPairsPopulate(t *testing.T) {
 	e := newMockEx()
 	ap := assetPairs{}
-	err := ap.populate(asset.Spot, e)
+	err := ap.populate(e, asset.Spot)
 	require.NoError(t, err)
 	require.NotEmpty(t, ap)
 	assert.Equal(t, 3, len(ap[asset.Spot]), "populate should return correct number of pairs for spot")
 	assert.Equal(t, "BTC-USDT", ap[asset.Spot][0].String(), "populate should respect format and sort the pairs")
-	err = ap.populate(asset.Futures, e)
+	err = ap.populate(e, asset.Futures)
 	require.NoError(t, err)
 	assert.Equal(t, 2, len(ap[asset.Futures]), "populate should return correct number of pairs for futures")
 
 	exp := errors.New("expected error")
 	e.errFormat = exp
-	err = ap.populate(asset.Spot, e)
+	err = ap.populate(e, asset.Spot)
 	assert.ErrorIs(t, err, exp, "populate should error correctly on format error")
 
 	e.pairs = assetPairs{}
 	ap = assetPairs{}
-	err = ap.populate(asset.Spot, e)
+	err = ap.populate(e, asset.Spot)
 	require.NoError(t, err, "populate must not error with no pairs enabled")
 	assert.Empty(t, ap, "populate should return an empty map with no pairs enabled")
 }
