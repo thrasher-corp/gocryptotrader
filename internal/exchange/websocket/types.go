e7fdc490bab14d094851df9abb7e0e60bd2f5448
Gareth Kirwan <gbjkirwan@gmail.com>
2025-03-07 11:24:10 +0700

    Websocket: Minor field comment and alignment in types

--- internal/exchange/websocket/types.go
+++ internal/exchange/websocket/types.go
@@ -32,21 +32,43 @@ const (
 
 // Manager provides connection and subscription management and routing
 type Manager struct {
-	canUseAuthenticatedEndpoints atomic.Bool
-	enabled                      atomic.Bool
-	state                        atomic.Uint32
-	verbose                      bool
-	connectionMonitorRunning     atomic.Bool
-	trafficTimeout               time.Duration
-	connectionMonitorDelay       time.Duration
-	proxyAddr                    string
-	defaultURL                   string
-	defaultURLAuth               string
-	runningURL                   string
-	runningURLAuth               string
-	exchangeName                 string
-	m                            sync.Mutex
-	connector                    func() error
+	enabled                       atomic.Bool
+	state                         atomic.Uint32
+	verbose                       bool
+	canUseAuthenticatedEndpoints  atomic.Bool
+	connectionMonitorRunning      atomic.Bool
+	trafficTimeout                time.Duration
+	connectionMonitorDelay        time.Duration
+	proxyAddr                     string
+	defaultURL                    string
+	defaultURLAuth                string
+	runningURL                    string
+	runningURLAuth                string
+	exchangeName                  string
+	features                      *protocol.Features
+	m                             sync.Mutex
+	connections                   map[Connection]*ConnectionWrapper
+	subscriptions                 *subscription.Store
+	connector                     func() error
+	rateLimitDefinitions          request.RateLimitDefinitions // rate limiters shared between Websocket and REST connections
+	Subscriber                    func(subscription.List) error
+	Unsubscriber                  func(subscription.List) error
+	GenerateSubs                  func() (subscription.List, error)
+	useMultiConnectionManagement  bool
+	DataHandler                   chan any
+	ToRoutine                     chan any
+	Match                         *Match
+	ShutdownC                     chan struct{}
+	Wg                            sync.WaitGroup
+	Orderbook                     buffer.Orderbook
+	Trade                         trade.Trade // Trade is a notifier for trades
+	Fills                         fill.Fills  // Fills is a notifier for fills
+	TrafficAlert                  chan struct{}
+	ReadMessageErrors             chan error
+	Conn                          Connection // Public connection
+	AuthConn                      Connection // Authenticated Private connection
+	ExchangeLevelReporter         Reporter   // Latency reporter
+	MaxSubscriptionsPerConnection int
 
 	// connectionManager stores all *potential* connections for the exchange, organised within ConnectionWrapper structs.
 	// Each ConnectionWrapper one connection (will be expanded soon) tailored for specific exchange functionalities or asset types. // TODO: Expand this to support multiple connections per ConnectionWrapper
@@ -54,58 +76,6 @@ type Manager struct {
 	// for exchanges that differentiate between trading pairs by using different connection endpoints or protocols for various asset classes.
 	// If an exchange does not require such differentiation, all connections may be managed under a single ConnectionWrapper.
 	connectionManager []*ConnectionWrapper
-	// connections holds a look up table for all connections to their corresponding ConnectionWrapper and subscription holder
-	connections map[Connection]*ConnectionWrapper
-
-	subscriptions *subscription.Store
-
-	// Subscriber function for exchange specific subscribe implementation
-	Subscriber func(subscription.List) error
-	// Subscriber function for exchange specific unsubscribe implementation
-	Unsubscriber func(subscription.List) error
-	// GenerateSubs function for exchange specific generating subscriptions from Features.Subscriptions, Pairs and Assets
-	GenerateSubs func() (subscription.List, error)
-
-	useMultiConnectionManagement bool
-
-	DataHandler chan any
-	ToRoutine   chan any
-
-	Match *Match
-
-	// shutdown synchronises shutdown event across routines
-	ShutdownC chan struct{}
-	Wg        sync.WaitGroup
-
-	// Orderbook is a local buffer of orderbooks
-	Orderbook buffer.Orderbook
-
-	// Trade is a notifier of occurring trades
-	Trade trade.Trade
-
-	// Fills is a notifier of occurring fills
-	Fills fill.Fills
-
-	// trafficAlert monitors if there is a halt in traffic throughput
-	TrafficAlert chan struct{}
-	// ReadMessageErrors will received all errors from ws.ReadMessage() and
-	// verify if its a disconnection
-	ReadMessageErrors chan error
-	features          *protocol.Features
-
-	Conn     Connection // Public connection
-	AuthConn Connection // Authenticated Privates connection
-
-	// Latency reporter
-	ExchangeLevelReporter Reporter
-
-	// MaxSubScriptionsPerConnection defines the maximum number of
-	// subscriptions per connection that is allowed by the exchange.
-	MaxSubscriptionsPerConnection int
-
-	// rateLimitDefinitions contains the rate limiters shared between Websocket and REST connections for all potential
-	// endpoints.
-	rateLimitDefinitions request.RateLimitDefinitions
 }
 
 // ManagerSetup defines variables for setting up a websocket manager
@@ -119,8 +89,6 @@ type ManagerSetup struct {
 	Unsubscriber          func(subscription.List) error
 	GenerateSubscriptions func() (subscription.List, error)
 	Features              *protocol.Features
-
-	// Local orderbook buffer config values
 	OrderbookBufferConfig buffer.Config
 
 	// UseMultiConnectionManagement allows the connections to be managed by the
@@ -129,12 +97,8 @@ type ManagerSetup struct {
 	UseMultiConnectionManagement bool
 
 	TradeFeed bool
-
-	// Fill data config values
 	FillsFeed bool
 
-	// MaxWebsocketSubscriptionsPerConnection defines the maximum number of
-	// subscriptions per connection that is allowed by the exchange.
 	MaxWebsocketSubscriptionsPerConnection int
 
 	// RateLimitDefinitions contains the rate limiters shared between WebSocket and REST connections for all endpoints.
@@ -148,36 +112,21 @@ type ManagerSetup struct {
 // WebsocketConnection contains all the data needed to send a message to a WS
 // connection
 type WebsocketConnection struct {
-	Verbose   bool
-	connected int32
-
-	// Gorilla websocket does not allow more than one goroutine to utilise writes methods
-	writeControl sync.Mutex
-
-	// RateLimit is a rate limiter for the connection itself
-	RateLimit *request.RateLimiterWithWeight
-	// RateLimitDefinitions contains the rate limiters shared between WebSocket and REST connections for all
-	// potential endpoints.
-	RateLimitDefinitions request.RateLimitDefinitions
-
-	ExchangeName string
-	URL          string
-	ProxyURL     string
-	Wg           *sync.WaitGroup
-	Connection   *underlying.Conn
-
-	// shutdown synchronises shutdown event across routines associated with this connection only e.g. ping handler
-	shutdown chan struct{}
-
-	Match             *Match
-	ResponseMaxLimit  time.Duration
-	Traffic           chan struct{}
-	readMessageErrors chan error
-
-	// bespokeGenerateMessageID is a function that returns a unique message ID
-	// defined externally. This is used for exchanges that require a unique
-	// message ID for each message sent.
+	Verbose                  bool
+	connected                int32
+	writeControl             sync.Mutex                     // Gorilla websocket does not allow more than one goroutine to utilise write methods
+	RateLimit                *request.RateLimiterWithWeight // RateLimit is a rate limiter for the connection itself
+	RateLimitDefinitions     request.RateLimitDefinitions   // RateLimitDefinitions contains the rate limiters shared between WebSocket and REST connections
+	Reporter                 Reporter
+	ExchangeName             string
+	URL                      string
+	ProxyURL                 string
+	Wg                       *sync.WaitGroup
+	Connection               *underlying.Conn
+	shutdown                 chan struct{}
+	Match                    *Match
+	ResponseMaxLimit         time.Duration
+	Traffic                  chan struct{}
+	readMessageErrors        chan error
 	bespokeGenerateMessageID func(highPrecision bool) int64
-
-	Reporter Reporter
 }
