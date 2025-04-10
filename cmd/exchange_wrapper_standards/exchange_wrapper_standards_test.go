77968cdda29d0acb604495cb02820250d7ebe34a
Ryan O'Hara-Reid <ryan.oharareid@thrasher.io>
2025-04-11 08:54:43 +1000

    shift over to websocket package error

--- cmd/exchange_wrapper_standards/exchange_wrapper_standards_test.go
+++ cmd/exchange_wrapper_standards/exchange_wrapper_standards_test.go
@@ -28,8 +28,8 @@ import (
 	"github.com/thrasher-corp/gocryptotrader/exchanges/order"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/orderbook"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/request"
-	"github.com/thrasher-corp/gocryptotrader/exchanges/stream"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/ticker"
+	"github.com/thrasher-corp/gocryptotrader/internal/exchange/websocket"
 	"github.com/thrasher-corp/gocryptotrader/portfolio/banking"
 	"github.com/thrasher-corp/gocryptotrader/portfolio/withdraw"
 )
@@ -650,7 +650,7 @@ var acceptableErrors = []error{
 	account.ErrExchangeHoldingsNotFound,
 	ticker.ErrTickerNotFound,
 	orderbook.ErrOrderbookNotFound,
-	stream.ErrNotConnected,
+	websocket.ErrNotConnected,
 }
 
 // warningErrors will t.Log(err) when thrown to diagnose things, but not necessarily suggest
