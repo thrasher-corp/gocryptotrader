1cf94ca9d3363914b675908cedcaa9648eaca2e0
Gareth Kirwan <gbjkirwan@gmail.com>
2025-05-20 09:52:13 +0700

    Dispatch: Replace nil checks with NilGuard

--- dispatch/dispatch_test.go
+++ dispatch/dispatch_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gofrs/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/thrasher-corp/gocryptotrader/common"
 )
 
 var (
@@ -33,10 +34,10 @@ func TestStartStop(t *testing.T) {
 	assert.False(t, d.isRunning(), "IsRunning should return false")
 
 	err := d.stop()
-	assert.ErrorIs(t, err, errDispatcherNotInitialized, "stop should error correctly")
+	assert.ErrorIs(t, err, common.ErrNilPointer, "stop should error correctly")
 
 	err = d.start(10, 0)
-	assert.ErrorIs(t, err, errDispatcherNotInitialized, "start should error correctly")
+	assert.ErrorIs(t, err, common.ErrNilPointer, "start should error correctly")
 
 	d = NewDispatcher()
 
@@ -74,7 +75,7 @@ func TestSubscribe(t *testing.T) {
 	t.Parallel()
 	var d *Dispatcher
 	_, err := d.subscribe(uuid.Nil)
-	assert.ErrorIs(t, err, errDispatcherNotInitialized, "subscribe should error correctly")
+	assert.ErrorIs(t, err, common.ErrNilPointer, "subscribe should error correctly")
 
 	d = NewDispatcher()
 
@@ -108,7 +109,7 @@ func TestUnsubscribe(t *testing.T) {
 	var d *Dispatcher
 
 	err := d.unsubscribe(uuid.Nil, nil)
-	assert.ErrorIs(t, err, errDispatcherNotInitialized, "unsubscribe should error correctly")
+	assert.ErrorIs(t, err, common.ErrNilPointer, "unsubscribe should error correctly")
 
 	d = NewDispatcher()
 
@@ -152,7 +153,7 @@ func TestPublish(t *testing.T) {
 	var d *Dispatcher
 
 	err := d.publish(uuid.Nil, nil)
-	assert.ErrorIs(t, err, errDispatcherNotInitialized, "publish should error correctly")
+	assert.ErrorIs(t, err, common.ErrNilPointer, "publish should error correctly")
 
 	d = NewDispatcher()
 
@@ -162,11 +163,11 @@ func TestPublish(t *testing.T) {
 	err = d.start(2, 10)
 	require.NoError(t, err, "start must not error")
 
-	err = d.publish(uuid.Nil, nil)
+	err = d.publish(uuid.Nil, "test")
 	assert.ErrorIs(t, err, errIDNotSet, "publish should error correctly")
 
 	err = d.publish(nonEmptyUUID, nil)
-	assert.ErrorIs(t, err, errNoData, "publish should error correctly")
+	assert.ErrorIs(t, err, common.ErrNilPointer, "publish should error correctly")
 
 	// demonstrate job limit error
 	d.routes[nonEmptyUUID] = []chan any{
@@ -209,7 +210,7 @@ func TestGetNewID(t *testing.T) {
 	var d *Dispatcher
 
 	_, err := d.getNewID(uuid.NewV4)
-	assert.ErrorIs(t, err, errDispatcherNotInitialized, "getNewID should error correctly")
+	assert.ErrorIs(t, err, common.ErrNilPointer, "getNewID should error correctly")
 
 	d = NewDispatcher()
 
@@ -233,16 +234,16 @@ func TestMux(t *testing.T) {
 	t.Parallel()
 	var mux *Mux
 	_, err := mux.Subscribe(uuid.Nil)
-	assert.ErrorIs(t, err, errMuxIsNil, "Subscribe should error correctly")
+	assert.ErrorIs(t, err, common.ErrNilPointer, "Subscribe should error correctly")
 
 	err = mux.Unsubscribe(uuid.Nil, nil)
-	assert.ErrorIs(t, err, errMuxIsNil, "Unsubscribe should error correctly")
+	assert.ErrorIs(t, err, common.ErrNilPointer, "Unsubscribe should error correctly")
 
 	err = mux.Publish(nil)
-	assert.ErrorIs(t, err, errMuxIsNil, "Publish should error correctly")
+	assert.ErrorIs(t, err, common.ErrNilPointer, "Publish should error correctly")
 
 	_, err = mux.GetID()
-	assert.ErrorIs(t, err, errMuxIsNil, "GetID should error correctly")
+	assert.ErrorIs(t, err, common.ErrNilPointer, "GetID should error correctly")
 
 	d := NewDispatcher()
 	err = d.start(0, 0)
@@ -251,7 +252,7 @@ func TestMux(t *testing.T) {
 	mux = GetNewMux(d)
 
 	err = mux.Publish(nil)
-	assert.ErrorIs(t, err, errNoData, "Publish should error correctly")
+	assert.ErrorIs(t, err, common.ErrNilPointer, "Publish should error correctly")
 
 	err = mux.Publish("lol")
 	assert.ErrorIs(t, err, errNoIDs, "Publish should error correctly")
