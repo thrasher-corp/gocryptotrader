e3b4c85bae8164b96de79b4ca75082e225c375bf
romanornr <romanornr@gmail.com>
2025-06-21 00:26:52 +0200

    signaler: improve cross-platform signal compatibility
    - Add runtime-based platform detection for signal handling
    - Include os.Kill only on Windows (cannot be caught/ignored on Unix)
    - Refactor signal initialization into separate getPlatformSignals function

--- signaler/signaler.go
+++ signaler/signaler.go
@@ -3,19 +3,36 @@ package signaler
 import (
 	"os"
 	"os/signal"
+	"runtime"
 	"syscall"
 )
 
 var s = make(chan os.Signal, 1)
 
 func init() {
-	sigs := []os.Signal{
+	sigs := getPlatformSignals()
+	signal.Notify(s, sigs...)
+}
+
+func getPlatformSignals() []os.Signal {
+	signals := []os.Signal{
 		os.Interrupt,
-		os.Kill,
 		syscall.SIGTERM,
 		syscall.SIGABRT,
 	}
-	signal.Notify(s, sigs...)
+
+	// Add os.Kill only for windows
+	// os.Kill cannot be caught or ignored on Unix-based systems
+	if runtime.GOOS == "windows" {
+		signals = append(signals, os.Kill)
+	}
+	return signals
+}
+
+// SignalNotifier is an interface for signal notification
+type SignalNotifier interface {
+	Notify(c chan<- os.Signal, sig ...os.Signal)
+	Stop(c chan<- os.Signal)
 }
 
 // WaitForInterrupt waits until a os.Signal is
