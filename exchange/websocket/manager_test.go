9dd6f6c9e26b33c8ee4f3308725a267572eab4b5
shazbert <ryan.oharareid@thrasher.io>
2025-07-01 11:29:20 +1000

    gk; nits assert value

--- exchange/websocket/manager_test.go
+++ exchange/websocket/manager_test.go
@@ -1349,5 +1349,5 @@ func TestWebsocketConnectionRequireMatchWithData(t *testing.T) {
 	err = ws.RequireMatchWithData(0, []byte("test"))
 	require.NoError(t, err)
 	require.Len(t, ch, 1, "must have one item in channel")
-	require.Equal(t, []byte("test"), <-ch)
+	assert.Equal(t, []byte("test"), <-ch)
 }
