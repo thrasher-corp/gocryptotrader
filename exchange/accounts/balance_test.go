af76bc2544322f328147a558a64f79e45819d370
Gareth Kirwan <gbjkirwan@gmail.com>
2025-07-23 12:48:24 +0200

    fixup! Accounts: Move singleton accounts service to exchange Accounts

--- exchange/accounts/balance_test.go
+++ exchange/accounts/balance_test.go
@@ -134,7 +134,7 @@ func TestBalanceUpdate(t *testing.T) {
 
 	n = time.Now()
 	u, err = b.update(Balance{UpdatedAt: n, Currency: currency.LTC, Total: 4.2})
-	assert.NoError(t, err, "should not error when Currency matches")
+	require.NoError(t, err, "should not error when Currency matches")
 	assert.Equal(t, n, b.internal.UpdatedAt, "should update UpdatedAt")
 	assert.False(t, u, "should return not updated when only time changed")
 
