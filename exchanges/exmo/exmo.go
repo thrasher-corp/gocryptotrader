1ea1072c9627ca26632d2fd4874d9a6013d65d98
Samuael A. <39623015+samuael@users.noreply.github.com>
2025-08-26 03:27:07 +0300

    exchanges: Limit mock test JSON data size by truncating slices and maps (#1968)
    * set limiter to first level mock data list and updated unit tests
    
    * address nested slices length limit
    
    * minor fix recording file and update unit tests
    
    * minor updates on unit tests
    
    * re-record mock files and minor fix on the unit tests ti adapt the mock data change
    
    * improve http recording limit value and fix issues with mock data in binance
    
    * added MockDataSliceLimit in request items and resolve minor unit test issues
    
    * resolve missed conflict
    
    * rename mock variables, resolve unit test issues, and other updates
    
    * minor fix to CheckJSON and update unit tests
    
    * minor unit test fix
    
    * further optimization on mock CheckJSON method, unit tests, and re-record poloniex
    
    * common and recording unit tests fix
    
    * minor linter issues fix
    
    * unit tests format fix
    
    * fix miscellaneous error
    
    * unit tests fix and minor docs update
    
    * re-record and reduce mock file size
    
    * indentation fix
    
    * minor assertion test fix
    
    * reverted log.Printf line in live testing
    
    * rename variables
    
    * update NewVCRServer unit test
    
    * replace string comparison with *net.OpError check
    
    * restructur net error test
    
    * exchanges/mock: Remove redundant error assertion message in TestNewVCRServer
    
    ---------
    
    Co-authored-by: Adrian Gallagher <adrian.gallagher@thrasher.io>

--- exchanges/exmo/exmo.go
+++ exchanges/exmo/exmo.go
@@ -293,12 +293,13 @@ func (e *Exchange) SendHTTPRequest(ctx context.Context, endpoint exchange.URL, p
 	}
 
 	item := &request.Item{
-		Method:        http.MethodGet,
-		Path:          urlPath + path,
-		Result:        result,
-		Verbose:       e.Verbose,
-		HTTPDebugging: e.HTTPDebugging,
-		HTTPRecording: e.HTTPRecording,
+		Method:                 http.MethodGet,
+		Path:                   urlPath + path,
+		Result:                 result,
+		Verbose:                e.Verbose,
+		HTTPDebugging:          e.HTTPDebugging,
+		HTTPRecording:          e.HTTPRecording,
+		HTTPMockDataSliceLimit: e.HTTPMockDataSliceLimit,
 	}
 	return e.SendPayload(ctx, request.Unset, func() (*request.Item, error) {
 		return item, nil
@@ -335,15 +336,16 @@ func (e *Exchange) SendAuthenticatedHTTPRequest(ctx context.Context, epath excha
 		headers["Content-Type"] = "application/x-www-form-urlencoded"
 
 		return &request.Item{
-			Method:        method,
-			Path:          path,
-			Headers:       headers,
-			Body:          strings.NewReader(payload),
-			Result:        result,
-			NonceEnabled:  true,
-			Verbose:       e.Verbose,
-			HTTPDebugging: e.HTTPDebugging,
-			HTTPRecording: e.HTTPRecording,
+			Method:                 method,
+			Path:                   path,
+			Headers:                headers,
+			Body:                   strings.NewReader(payload),
+			Result:                 result,
+			NonceEnabled:           true,
+			Verbose:                e.Verbose,
+			HTTPDebugging:          e.HTTPDebugging,
+			HTTPRecording:          e.HTTPRecording,
+			HTTPMockDataSliceLimit: e.HTTPMockDataSliceLimit,
 		}, nil
 	}, request.AuthenticatedRequest)
 }
