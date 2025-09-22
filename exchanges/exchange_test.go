126d746515faf4d87bf0665f2cfec3e84a7b994f
Ryan O'Hara-Reid <oharareid.ryan@gmail.com>
2025-09-22 10:12:29 +1000

    Update exchanges/exchange_test.go
    Co-authored-by: Gareth Kirwan <gbjkirwan@gmail.com>

--- exchanges/exchange_test.go
+++ exchanges/exchange_test.go
@@ -1098,9 +1098,8 @@ func TestUpdatePairs(t *testing.T) {
 		t.Fatalf("received: '%v' but expected: '%v'", false, true)
 	}
 
-	// purge all pairs
 	err = UAC.UpdatePairs(currency.Pairs{}, asset.Spot, true)
-	require.NoError(t, err)
+	require.NoError(t, err, "purging all pairs should not error")
 
 	pairs, err := UAC.GetEnabledPairs(asset.Spot)
 	require.NoError(t, err)
