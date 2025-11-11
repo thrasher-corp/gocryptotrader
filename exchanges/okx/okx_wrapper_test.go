816d81a9361a374b830cf57248a2b55d8f380e92
shazbert <ryan.oharareid@thrasher.io>
2025-11-11 13:26:31 +1100

    okx: Add length check in tests for string -> uuid conversion

--- exchanges/okx/okx_wrapper_test.go
+++ exchanges/okx/okx_wrapper_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/gofrs/uuid"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -14,7 +13,8 @@ func TestMessageID(t *testing.T) {
 	require.Len(t, id, 32, "Must return the correct length of message id")
 	u, err := uuid.FromString(id)
 	require.NoError(t, err, "MessageID must return a valid UUID")
-	assert.Equal(t, byte(0x7), u.Version(), "MessageID should return a V7 uuid")
+	require.Equal(t, byte(0x7), u.Version(), "MessageID must return a V7 uuid")
+	require.Len(t, u.String(), 36, "UUID v7 string representation must be 36 characters long")
 }
 
 // 7696807	       153.1 ns/op	      48 B/op	       2 allocs/op
