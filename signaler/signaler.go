cecfae9237cd4e15be6db228bd49cbc7e023dbbf
Romano <6548898+romanornr@users.noreply.github.com>
2025-11-17 03:40:23 +0100

    Signaler: fix comment
    Co-authored-by: Ryan O'Hara-Reid <oharareid.ryan@gmail.com>

--- signaler/signaler.go
+++ signaler/signaler.go
@@ -7,7 +7,7 @@ import (
 	"syscall"
 )
 
-// WaitForInterrupt returns a channel to receive termination signalls
+// WaitForInterrupt returns a channel to receive termination signals
 func WaitForInterrupt() chan os.Signal {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
