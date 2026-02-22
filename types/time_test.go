28255aeab9a0a7a332b6315f9a3b2bbb525a22da
Scott <scott.grant@thrasher.io>
2026-02-23 10:06:41 +1100

    HEY WHO SAID YOU COULD DISAPPEAR

--- types/time_test.go
+++ types/time_test.go
@@ -41,6 +41,7 @@ func TestUnmarshalJSON(t *testing.T) {
 		{`"123456"`, time.Time{}, errInvalidTimestampFormat},
 		{`"12345678"`, time.Time{}, errInvalidTimestampFormat},
 		{`"2025-03-28T08:00:00Z"`, time.Time{}, strconv.ErrSyntax}, // RFC3339 format (currently unsupported)
+		{`"1606292218213.45.8"`, time.Time{}, strconv.ErrSyntax},   // parse int failure
 	} {
 		t.Run(tc.input, func(t *testing.T) {
 			t.Parallel()
