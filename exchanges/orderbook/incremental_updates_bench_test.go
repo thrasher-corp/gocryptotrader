3b37dd00750d2a48189b9c4d0f2e7a58a16f49d9
Adrian Gallagher <adrian.gallagher@thrasher.io>
2026-09-16 16:15:01 +1000

    BitMEX: Remove exchange implementation (#2295)
    * BitMEX: Remove exchange implementation
    
    * orderbook: remove BitMEX-only insert action
    
    * orderbook: clarify update cleanup
    
    * orderbook: remove retired insert action

--- exchanges/orderbook/incremental_updates_bench_test.go
+++ exchanges/orderbook/incremental_updates_bench_test.go
@@ -5,20 +5,17 @@ import (
 	"time"
 )
 
-// Benchstat medians for PR base to measured production head
-// (20 counterbalanced fresh-process observations per revision):
-// Before: 5021.0 ns/op  10880 B/op  1 allocs/op; After: 688.1 ns/op  0 B/op  0 allocs/op
-func BenchmarkProcessUpdateInsertDelete(b *testing.B) {
+func BenchmarkProcessUpdateUpdateOrInsertDelete(b *testing.B) {
 	depth := NewDepth(id)
 	if err := depth.LoadSnapshot(newSnapshot(256)); err != nil {
 		b.Fatal(err)
 	}
 
 	updateTime := time.Unix(1, 0)
-	insertUpdate := &Update{
+	updateOrInsertUpdate := &Update{
 		UpdateTime: updateTime,
 		Asks:       Levels{{Price: 1465.5, Amount: 2, ID: 1000}},
-		Action:     InsertAction,
+		Action:     UpdateOrInsertAction,
 	}
 	deleteUpdate := &Update{
 		UpdateTime: updateTime,
@@ -26,7 +23,7 @@ func BenchmarkProcessUpdateInsertDelete(b *testing.B) {
 		Action:     DeleteAction,
 	}
 	for b.Loop() {
-		if err := depth.ProcessUpdate(insertUpdate); err != nil {
+		if err := depth.ProcessUpdate(updateOrInsertUpdate); err != nil {
 			b.Fatal(err)
 		}
 		if err := depth.ProcessUpdate(deleteUpdate); err != nil {
